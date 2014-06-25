ff804b3c0c086f4e5ba99d81c6ba7c09c1ddb3e1
Tom Purucker <purucker.tom@gmail.com>
2014-06-25 15:38:53 -0400

    make sphinx pretty

--- docs/conf.py
+++ docs/conf.py
@@ -123,12 +123,18 @@ numpydoc_show_class_members = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+#html_theme = 'default'
+#html_theme = 'agogo'
+html_theme = 'nature'
+#html_theme = 'pyramid'
+#html_theme = 'sphinxdoc'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+#  "rightsidebar":"True"
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []

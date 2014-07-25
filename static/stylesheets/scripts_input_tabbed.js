5e526f5f6f7c3d92a5abc026258407121238fcda
Jon Flash <funkswing@gmail.com>
2014-07-25 12:26:21 -0400

    small edit

--- static/stylesheets/scripts_input_tabbed.js
+++ static/stylesheets/scripts_input_tabbed.js
@@ -106,10 +106,6 @@ $( document ).ready(function() {
 
 	// Assign class to be jQueryUI date picker widget
 	$( ".datePicker" ).datepicker();
-	// on mousedown event
-	// $('.selectDay a').mousedown(function() {
-	// 	$(this).addClass('ui-state-default-onClick');
-	// });
 
 });
 

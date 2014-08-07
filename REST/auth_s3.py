7cdbb03baa02627a016564c05e1781c5d86e57ca
Jon Flash <funkswing@gmail.com>
2014-08-07 17:56:40 -0400

    Fix indentation

--- REST/auth_s3.py
+++ REST/auth_s3.py
@@ -1,3 +1,5 @@
+import os
+
 def setHTTPHeaders():
 	"""
 	If S3 requires authentication provide the key
@@ -7,11 +9,11 @@ def setHTTPHeaders():
 		import base64
 		import keys_Picloud_S3
 		
-    	api_key=keys_Picloud_S3.picloud_api_key
+		api_key=keys_Picloud_S3.picloud_api_key
 		api_secretkey=keys_Picloud_S3.picloud_api_secretkey
 		base64string = base64.encodestring('%s:%s' % (api_key, api_secretkey))[:-1]
 		http_headers = {'Authorization' : 'Basic %s' % base64string, 'Content-Type' : 'application/json'}	
 	else:
-    	http_headers = {'Content-Type' : 'application/json'}
+		http_headers = {'Content-Type' : 'application/json'}
 
 	return http_headers

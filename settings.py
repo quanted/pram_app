32427a5e3c05bc7b0d42e89f9ae8cdb96fc3344b
Jon Flash <funkswing@gmail.com>
2014-09-09 14:17:36 -0400

    Updated REST server calls

--- settings.py
+++ settings.py
@@ -21,8 +21,8 @@ os.environ.update({
     'UBERTOOL_BATCH_SERVER': 'http://uberrest-topknotmeadows.rhcloud.com/',
     'UBERTOOL_MONGO_SERVER': 'http://uberrest-topknotmeadows.rhcloud.com',
     'UBERTOOL_SECURE_SERVER': 'http://uberrest-topknotmeadows.rhcloud.com',   
-    'UBERTOOL_REST_SERVER': 'http://localhost:80',                         # Local REST server
-    #'UBERTOOL_REST_SERVER': 'http://54.83.18.251:80',                      # Tao's EC2 REST server 
+    #'UBERTOOL_REST_SERVER': 'http://localhost:80',                         # Local REST server
+    'UBERTOOL_REST_SERVER': 'http://54.83.18.251:80',                      # Tao's EC2 REST server 
     #'UBERTOOL_REST_SERVER': 'http://54.210.118.56'                         # EB Pilot REST server
     # 'UBERTOOL_REST_SERVER': 'http://'                           # New EB Pilot REST server
     'PROJECT_PATH': PROJECT_ROOT,

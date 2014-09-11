cb869965c53c799b4a10074122dc56e9b36e3eb5
funkswing <funkswing@gmail.com>
2014-09-11 13:06:02 -0700

    Updated apache django settings

--- settings_apache.py
+++ settings_apache.py
@@ -22,9 +22,9 @@ os.environ.update({
     'UBERTOOL_MONGO_SERVER': 'http://uberrest-topknotmeadows.rhcloud.com',
     'UBERTOOL_SECURE_SERVER': 'http://uberrest-topknotmeadows.rhcloud.com',   
     #'UBERTOOL_REST_SERVER': 'http://localhost:80',                         # Local REST server
-    'UBERTOOL_REST_SERVER': 'http://54.83.18.251:80',                      # Tao's EC2 REST server 
+    #'UBERTOOL_REST_SERVER': 'http://54.83.18.251:80',                      # Tao's EC2 REST server 
     #'UBERTOOL_REST_SERVER': 'http://54.210.118.56'                         # EB Pilot REST server
-    # 'UBERTOOL_REST_SERVER': 'http://',                           # New EB Pilot REST server
+    'UBERTOOL_REST_SERVER': 'http://172.20.100.13:7777',                           # CGI Internal
     'PROJECT_PATH': PROJECT_ROOT,
     'SITE_SKIN': 'EPA'                          # Leave empty ('') for default skin, 'EPA' for EPA skin
 })

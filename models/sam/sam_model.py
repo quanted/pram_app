4b268c86bf2513b1c4eda7658c8499ae383ddf08
Jon Flash <funkswing@gmail.com>
2014-10-23 17:22:11 -0400

    SAM Output Page for Demo Update

--- models/sam/sam_model.py
+++ models/sam/sam_model.py
@@ -17,32 +17,33 @@ http_headers = auth_s3.setHTTPHeaders()
 url_part1 = os.environ['UBERTOOL_REST_SERVER']
 
 
-def get_jid(run_type, chemical_name, koc, soil_metabolism_hl, crop, crop_number, noa, app_method, application_rate, refine, refine_time_window, refine_percent_applied, region, sim_type, sim_date_start, sim_date_end, sim_date_1stapp, output_type, output_tox, output_tox_value, output_format):
+# def get_jid(run_type, chemical_name, koc, soil_metabolism_hl, crop, crop_number, noa, app_method, application_rate, refine, refine_time_window, refine_percent_applied, region, sim_type, sim_date_start, sim_date_end, sim_date_1stapp, output_type, output_tox, output_tox_value, output_format):
+def get_jid(run_type):
     """
     Generate job ID and send inputs to JSON-RPC model server
     """
     all_dic = {
-        "run_type" : run_type,
-        "chemical_name" : chemical_name,
-        "koc" : koc,
-        "soil_metabolism_hl" : soil_metabolism_hl,
-        "crop" : crop,
-        "crop_number" : crop_number,
-        "noa" : noa,
-        "app_method" : app_method,
-        "application_rate" : application_rate,
-        "refine" : refine,
-        "refine_time_window" : refine_time_window,
-        "refine_percent_applied" : refine_percent_applied,
-        "region" : region,
-        "sim_type" : sim_type,
-        "sim_date_start" : sim_date_start,
-        "sim_date_end" : sim_date_end,
-        "sim_date_1stapp" : sim_date_1stapp,
-        "output_type" : output_type,
-        "output_tox" : output_tox,
-        "output_tox_value" : output_tox_value,
-        "output_format" : output_format,
+        # "run_type" : run_type,
+        # "chemical_name" : chemical_name,
+        # "koc" : koc,
+        # "soil_metabolism_hl" : soil_metabolism_hl,
+        # "crop" : crop,
+        # "crop_number" : crop_number,
+        # "noa" : noa,
+        # "app_method" : app_method,
+        # "application_rate" : application_rate,
+        # "refine" : refine,
+        # "refine_time_window" : refine_time_window,
+        # "refine_percent_applied" : refine_percent_applied,
+        # "region" : region,
+        # "sim_type" : sim_type,
+        # "sim_date_start" : sim_date_start,
+        # "sim_date_end" : sim_date_end,
+        # "sim_date_1stapp" : sim_date_1stapp,
+        # "output_type" : output_type,
+        # "output_tox" : output_tox,
+        # "output_tox_value" : output_tox_value,
+        # "output_format" : output_format,
     }
     data = json.dumps(all_dic)
     logger.info(data)
@@ -60,32 +61,37 @@ def convert_dict_key(key):
         return key
 
 class SAM(object):
-    def __init__(self, run_type, chemical_name, koc, soil_metabolism_hl, crop, crop_number, noa, app_method, application_rate, refine, refine_time_window, refine_percent_applied, region, sim_type, sim_date_start, sim_date_end, sim_date_1stapp, output_type, output_tox, output_tox_value, output_format):
+    # def __init__(self, run_type, chemical_name, koc, soil_metabolism_hl, crop, crop_number, noa, app_method, application_rate, refine, refine_time_window, refine_percent_applied, region, sim_type, sim_date_start, sim_date_end, sim_date_1stapp, output_type, output_tox, output_tox_value, output_format):
 
-        # Create class lists for input variables
+    #     # Create class lists for input variables
+    #     self.run_type = run_type
+    #     self.chemical_name = chemical_name
+    #     self.koc = koc
+    #     self.soil_metabolism_hl = soil_metabolism_hl
+    #     self.crop = crop
+    #     self.crop_number = crop_number
+    #     self.noa = noa
+    #     self.app_method = app_method
+    #     self.application_rate = application_rate
+    #     self.refine = refine
+    #     self.refine_time_window = refine_time_window
+    #     self.refine_percent_applied = refine_percent_applied
+    #     self.region = region
+    #     self.sim_type = sim_type
+    #     self.sim_date_start = sim_date_start
+    #     self.sim_date_end = sim_date_end
+    #     self.sim_date_1stapp = sim_date_1stapp
+    #     self.output_type = output_type
+    #     self.output_tox = output_tox
+    #     self.output_tox_value = output_tox_value
+    #     self.output_format = output_format
+
+    def __init__(self, run_type, scenario_selection):
         self.run_type = run_type
-        self.chemical_name = chemical_name
-        self.koc = koc
-        self.soil_metabolism_hl = soil_metabolism_hl
-        self.crop = crop
-        self.crop_number = crop_number
-        self.noa = noa
-        self.app_method = app_method
-        self.application_rate = application_rate
-        self.refine = refine
-        self.refine_time_window = refine_time_window
-        self.refine_percent_applied = refine_percent_applied
-        self.region = region
-        self.sim_type = sim_type
-        self.sim_date_start = sim_date_start
-        self.sim_date_end = sim_date_end
-        self.sim_date_1stapp = sim_date_1stapp
-        self.output_type = output_type
-        self.output_tox = output_tox
-        self.output_tox_value = output_tox_value
-        self.output_format = output_format
+        self.scenario_selection = scenario_selection
 
+        # self.final_res = get_jid(self.run_type, self.chemical_name, self.koc, self.soil_metabolism_hl, self.crop, self.crop_number, self.noa, self.app_method, self.application_rate, self.refine, self.refine_time_window, self.refine_percent_applied, self.region, self.sim_type, self.sim_date_start, self.sim_date_end, self.sim_date_1stapp, self.output_type, self.output_tox, self.output_tox_value, self.output_format)
+        self.final_res = get_jid(self.run_type)
 
-        self.final_res = get_jid(self.run_type, self.chemical_name, self.koc, self.soil_metabolism_hl, self.crop, self.crop_number, self.noa, self.app_method, self.application_rate, self.refine, self.refine_time_window, self.refine_percent_applied, self.region, self.sim_type, self.sim_date_start, self.sim_date_end, self.sim_date_1stapp, self.output_type, self.output_tox, self.output_tox_value, self.output_format)
         self.jid = self.final_res[0]
         self.link = self.final_res[1][0]
--- models/sam/sam_tables.py
+++ models/sam/sam_tables.py
@@ -70,67 +70,187 @@ tmpl = Template(djtemplate)
 # Table Initialization #
 ########################
 def gett1data(sam_obj):
+    
+    if (sam_obj.scenario_selection == '1'):
+        scenario = 'Atrazine - Corn'
+        chemical_name = 'Atrazine'
+        koc = '100'
+        soil_hl = '123'
+    elif (sam_obj.scenario_selection == '2'):
+        scenario = 'Chlorpyrifos - Corn'
+        chemical_name = 'Chlorpyrifos'
+        koc = '6040'
+        soil_hl = '109'
+    elif (sam_obj.scenario_selection == '3'):
+        scenario = 'Chlorpyrifos - Soybeans'
+        chemical_name = 'Chlorpyrifos'
+        koc = '6040'
+        soil_hl = '109'
+    elif (sam_obj.scenario_selection == '4'):
+        scenario = 'Fipronil - Corn'
+        chemical_name = 'Fipronil'
+        koc = '727'
+        soil_hl = '128'
+    elif (sam_obj.scenario_selection == '5'):
+        scenario = 'Metolachlor - Corn'
+        chemical_name = 'Metolachlor'
+        koc = '181'
+        soil_hl = '49'
+    else:
+        scenario = 'n/a'
+        chemical_name = 'n/a'
+        koc = 'n/a'
+        soil_hl = 'n/a'    
+
     data = {
-        "Parameter": ['Chemical Name', 'Koc', 'Soil Metabolism Halflife'],
-        "Value": ['%s' % sam_obj.chemical_name, sam_obj.koc, '%s' % sam_obj.soil_metabolism_hl],
-        "Units": ['', 'mL/g', 'days']
+        "Parameter": ['Scenario', 'Chemical Name', 'Koc', 'Soil Metabolism Halflife'],
+        "Value": [scenario, chemical_name, koc, soil_hl],
+        "Units": ['', '', 'mL/g', 'days']
     }
+    # data = {
+    #     "Parameter": ['Chemical Name', 'Koc', 'Soil Metabolism Halflife'],
+    #     "Value": ['%s' % sam_obj.chemical_name, sam_obj.koc, '%s' % sam_obj.soil_metabolism_hl],
+    #     "Units": ['', 'mL/g', 'days']
+    # }
     return data
 
 def gett2data(sam_obj):
     # Convert tuple into dictionary
-    CROP_CHOICES = dict(sam_parameters.SamInp_app.CROP_CHOICES)
+    # CROP_CHOICES = dict(sam_parameters.SamInp_app.CROP_CHOICES)
     
-    try:
-        app_method = sam_parameters.SamInp_app.APP_METH_CHOICES[int(sam_obj.app_method) - 1][1]
-    except:
-        app_method = ""
+    # try:
+    #     app_method = sam_parameters.SamInp_app.APP_METH_CHOICES[int(sam_obj.app_method) - 1][1]
+    # except:
+    #     app_method = ""
 
-    try:
-        refine = sam_parameters.SamInp_app_refine.REFINEMENT_CHOICES[int(sam_obj.refine) - 1][1]
-    except:
-        refine = ""
-
-    data = {
-        "Parameter": ['Crop', 'Total Number of Crops', 'Total Number of Applications',
-                        'Application method', 'Application Rate', 'Refinements',
-                        'Time Window', 'Percent Applied' ],
-        "Value": ['%s' % CROP_CHOICES[int(sam_obj.crop)], sam_obj.crop_number, '%s' % sam_obj.noa,
-                    '%s' % app_method, sam_obj.application_rate, '%s' % refine,
-                    '%s' % sam_obj.refine_time_window, sam_obj.refine_percent_applied ],
-        "Units": ['', '', '', '', 'kg/ha', '', 'days', '']
-    }
+    # try:
+    #     refine = sam_parameters.SamInp_app_refine.REFINEMENT_CHOICES[int(sam_obj.refine) - 1][1]
+    # except:
+    #     refine = ""
+    no_of_crops = '4'
+    app_meth = 'Ground'
+    if (sam_obj.scenario_selection == '1'):
+        crop = 'Corn, Corn/grains, Corn/wheat, Corn/soybeans'
+        noa = '1500'
+        app_rate = '1.3'
+        refinements = 'Uniform Step Application over Window'
+        time_win = '7'
+        percent_app = '50'
+        time_win2 = '43'
+        percent_app2 = '50'
+    elif (sam_obj.scenario_selection == '2'):
+        crop = 'Corn, Corn/grains, Corn/wheat, Corn/soybeans'
+        noa = '900'
+        app_rate = '1.1'
+        refinements = 'Uniform Application over Window'
+        time_win = '30'
+        percent_app = '100'
+    elif (sam_obj.scenario_selection == '3'):
+        crop = 'Soybeans, Soybeans/grains, Soybeans/wheat, Soybeans/cotton'
+        noa = '1260'
+        app_rate = '1.1'
+        refinements = 'Uniform Application over Window'
+        time_win = '42'
+        percent_app = '100'
+    elif (sam_obj.scenario_selection == '4'):
+        crop = 'Corn, Corn/grains, Corn/wheat, Corn/soybeans'
+        noa = '1500'
+        app_rate = '0.1'
+        refinements = 'Uniform Step Application over Window'
+        time_win = '7'
+        percent_app = '50'
+        time_win2 = '43'
+        percent_app2 = '50'
+    elif (sam_obj.scenario_selection == '5'):
+        crop = 'Corn, Corn/grains, Corn/wheat, Corn/soybeans'
+        noa = '1500'
+        app_rate = '1.05'
+        refinements = 'Uniform Step Application over Window'
+        time_win = '7'
+        percent_app = '50'
+        time_win2 = '43'
+        percent_app2 = '50'
+    else:
+        crop = 'n/a'
+        no_of_crops = 'n/a'
+        noa = 'n/a'
+        app_meth = 'n/a'
+        app_rate = 'n/a'
+        refinements = 'n/a'
+        time_win = 'n/a'
+        percent_app = 'n/a'
+    
+    if (refinements == 'Uniform Step Application over Window'):
+        data = {
+            "Parameter": ['Crop', 'Total Number of Crops', 'Total Number of Applications',
+                            'Application method', 'Application Rate', 'Refinements',
+                            'Time Window #1', 'Percent Applied #1', 'Time Window #2', 'Percent Applied #2' ],
+            "Value": [crop, no_of_crops, noa,
+                        app_meth, app_rate, refinements,
+                        time_win, percent_app, time_win2, percent_app2 ],
+            "Units": ['', '', '', '', 'kg/ha', '', 'days', '%', 'days', '%']
+        }
+    else:
+        data = {
+            "Parameter": ['Crop', 'Total Number of Crops', 'Total Number of Applications',
+                            'Application method', 'Application Rate', 'Refinements',
+                            'Time Window', 'Percent Applied' ],
+            "Value": [crop, no_of_crops, noa,
+                        app_meth, app_rate, refinements,
+                        time_win, percent_app ],
+            "Units": ['', '', '', '', 'kg/ha', '', 'days', '%']
+        }
+    # data = {
+    #     "Parameter": ['Crop', 'Total Number of Crops', 'Total Number of Applications',
+    #                     'Application method', 'Application Rate', 'Refinements',
+    #                     'Time Window', 'Percent Applied' ],
+    #     "Value": ['%s' % CROP_CHOICES[int(sam_obj.crop)], sam_obj.crop_number, '%s' % sam_obj.noa,
+    #                 '%s' % app_method, sam_obj.application_rate, '%s' % refine,
+    #                 '%s' % sam_obj.refine_time_window, sam_obj.refine_percent_applied ],
+    #     "Units": ['', '', '', '', 'kg/ha', '', 'days', '']
+    # }
     return data
 
 def gett3data(sam_obj):
-    try:
-        sim_type = sam_parameters.SamInp_sim.SIM_CHOICES[int(sam_obj.sim_type) - 1][1]
-    except:
-        sim_type = ""
+    # try:
+    #     sim_type = sam_parameters.SamInp_sim.SIM_CHOICES[int(sam_obj.sim_type) - 1][1]
+    # except:
+    #     sim_type = ""
     
     data = {
         "Parameter": ['Sate/Region', 'Simulation Type', 'Start Date', 'End Date', 'First Application Date'],
-        "Value": ['%s' % sam_obj.region, sim_type,
-                    '%s' % sam_obj.sim_date_start, '%s' % sam_obj.sim_date_end, '%s' % sam_obj.sim_date_1stapp ]
+        "Value": [ 'Ohio Valley', 'Eco', '01/01/1984', '12/31/2013', '04/20/1984' ]
     }
+
+    # data = {
+    #     "Parameter": ['Sate/Region', 'Simulation Type', 'Start Date', 'End Date', 'First Application Date'],
+    #     "Value": ['%s' % sam_obj.region, sim_type,
+    #                 '%s' % sam_obj.sim_date_start, '%s' % sam_obj.sim_date_end, '%s' % sam_obj.sim_date_1stapp ]
+    # }
     return data
 
 def gett4data(sam_obj):
-    try:
-        output_type = sam_parameters.SamInp_output.OUTPUT_TYPE_CHOICES[int(sam_obj.output_type) - 1][1]
-    except:
-        output_type = ""
+    # try:
+    #     output_type = sam_parameters.SamInp_output.OUTPUT_TYPE_CHOICES[int(sam_obj.output_type) - 1][1]
+    # except:
+    #     output_type = ""
 
-    try:
-        output_tox = sam_parameters.SamInp_output.TOX_PERIOD_CHOICES[int(sam_obj.output_tox) - 1][1]
-    except:
-        output_tox = ""
+    # try:
+    #     output_tox = sam_parameters.SamInp_output.TOX_PERIOD_CHOICES[int(sam_obj.output_tox) - 1][1]
+    # except:
+    #     output_tox = ""
 
     data = {
-        "Parameter": ['Output Preference', 'Threshold Time Period', 'Threshold', 'Output Format'],
-        "Value": ['%s' % output_type, output_tox, '%s' % sam_obj.output_tox_value, '%s' % sam_obj.output_format],
-        "Units": ['', '', mark_safe('&micro;g/L'), '']
+        "Parameter": ['Output Preference', 'Output Format'],
+        "Value": ['Daily Concentrations', 'Table & Map'],
+        "Units": ['', '']
     }
+
+    # data = {
+    #     "Parameter": ['Output Preference', 'Threshold Time Period', 'Threshold', 'Output Format'],
+    #     "Value": ['%s' % output_type, output_tox, '%s' % sam_obj.output_tox_value, '%s' % sam_obj.output_format],
+    #     "Units": ['', '', mark_safe('&micro;g/L'), '']
+    # }
     return data
 
 
@@ -213,10 +333,25 @@ def table_all(sam_obj):
     html = html + table_2(sam_obj)
     html = html + table_3(sam_obj)
     html = html + table_4(sam_obj)
+    
+    if (sam_obj.scenario_selection == '1'):
+        link = 'https://s3.amazonaws.com/super_przm/SAM_IB2QZS.zip'
+    elif (sam_obj.scenario_selection == '2'):
+        link = ''
+    elif (sam_obj.scenario_selection == '3'):
+        link = ''
+    elif (sam_obj.scenario_selection == '4'):
+        link = ''
+    elif (sam_obj.scenario_selection == '5'):
+        link = ''
+    else:
+        link = ''
+
     html = html + """
+    <br>
     <H3 class="out_3 collapsible" id="section1"><span></span>Model Outputs</H3>
     <div class="out_3">
-        <H4 class="out_1 collapsible" id="section1"><span></span>Download</H4>
+        <H4 class="out_1 collapsible" id="section1"><span></span>Download Model Output</H4>
             <div class="out_ container_output">
                 <table class="out_">
                     <tr>
@@ -229,5 +364,8 @@ def table_all(sam_obj):
                     </tr>
                 </table>
             </div>
-    </div>""" %sam_obj.link
+    """ % link
+
+    html = html + render_to_string('sam_mapping_demo.html', {})
+
     return html

eeb061d6ec4270ddee06945d3878cfcfca8f3c7b
Jon Flash <funkswing@gmail.com>
2015-04-03 15:27:59 -0400

    SAM Demo April 2015, adding missing files

---
+++ static/stylesheets/scripts_geoserver_polling.js
@@ -0,0 +1,47 @@
+// Long polling for SAM results
+$( document ).ready(function() {
+
+    $('.sam_map').hide();
+
+    // Warn user not to refresh page
+    alert('SAM run successfully submitted.  Do NOT refresh page.  ' +
+        'This will result in duplicate model submissions and will slow down returning of model results.');
+
+    var jid_ajax = document.getElementById('jid').innerHTML // SAM run 'jid'
+
+    function updateTimer() {
+
+        $.ajax({ 
+            url: "/geoserver/sam_done/" + jid_ajax,
+            method: 'POST',
+            // dataType: "text",
+            success: function(data) {
+                
+                // console.log(data);
+
+                if (data == "done") {
+                    showMap();
+                } else {
+                    setTimeout(updateTimer, 10000); // poll every 10s until success
+                }
+
+            },
+            error: function( jqXHR, textStatus, errorThrown ){
+                console.log(jqXHR);
+                console.log(textStatus);
+                console.log(errorThrown);
+
+                setTimeout(updateTimer, 10000); // poll every 10s until success
+            }
+
+        });
+
+    }
+
+    function showMap() {
+        $('#sam_still_working').hide();
+        $('.sam_map').show();
+    }
+
+    updateTimer(); //initiate the timer
+});

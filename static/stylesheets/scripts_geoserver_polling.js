fc162870059550643b96201e93137a143078f9c8
Jon Flash <funkswing@gmail.com>
2015-04-20 17:53:42 -0400

    SAM Demo April 2015 Test, Output Map coloring, update

--- static/stylesheets/scripts_geoserver_polling.js
+++ static/stylesheets/scripts_geoserver_polling.js
@@ -44,7 +44,7 @@ $( document ).ready(function() {
     function showMap() {
 
         sam_output_layer = new OpenLayers.Layer.WMS(
-            "cite:huc12s05 - Tiled",
+            "SAM SQL View",
             "http://134.67.114.4/geoserver/cite/wms",
             {
                 "LAYERS": 'cite:joinTest',
@@ -64,6 +64,51 @@ $( document ).ready(function() {
         map.removeLayer(tiled);
         map.addLayers([sam_output_layer]);
         console.log("Tried to switch baselayer");
+
+        // support GetFeatureInfo
+        map.events.register('click', map, function (e) {
+
+            document.getElementById('nodelist').innerHTML = "Loading... please wait...";
+            var params = {
+                REQUEST: "GetFeatureInfo",
+                EXCEPTIONS: "application/vnd.ogc.se_xml",
+                BBOX: map.getExtent().toBBOX(),
+                SERVICE: "WMS",
+                INFO_FORMAT: 'application/json',  // If set to 'text/html', returned HTML is formatted using templates on Geoserver
+                QUERY_LAYERS: map.layers[0].params.LAYERS,
+                FEATURE_COUNT: 50,
+                "Layers": 'cite:joinTest',
+                WIDTH: map.size.w,
+                HEIGHT: map.size.h,
+                format: format,
+                //styles: map.layers[0].params.STYLES,
+                srs: map.layers[0].params.SRS};
+
+            // handle the wms 1.3 vs wms 1.1 madness
+            if(map.layers[0].params.VERSION == "1.3.0") {
+                params.version = "1.3.0";
+                params.j = parseInt(e.xy.x);
+                params.i = parseInt(e.xy.y);
+            } else {
+                params.version = "1.1.1";
+                params.x = parseInt(e.xy.x);
+                params.y = parseInt(e.xy.y);
+            }
+
+            // merge filters
+            if(map.layers[0].params.CQL_FILTER != null) {
+                params.cql_filter = map.layers[0].params.CQL_FILTER;
+            }
+            if(map.layers[0].params.FILTER != null) {
+                params.filter = map.layers[0].params.FILTER;
+            }
+            if(map.layers[0].params.FEATUREID) {
+                params.featureid = map.layers[0].params.FEATUREID;
+            }
+            //                                                                 caller onComplete onFailure
+            OpenLayers.loadURL("http://134.67.114.4/geoserver/cite/wms", params, this, setHTML, setHTML);
+            OpenLayers.Event.stop(e);
+        });
     }
 
     updateTimer(); //initiate the timer

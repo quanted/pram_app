28629968ba4100fc52ae7b602328fcc248823037
Jon Flash <funkswing@gmail.com>
2015-08-18 17:22:52 -0400

    ORE: Error handling

--- models/ore/ore_rest_calls.py
+++ models/ore/ore_rest_calls.py
@@ -130,7 +130,7 @@ def output_export(request):
     else:
         return HttpResponse(
             json.dumps( {
-                'error': 'Error processing request.',
+                'error': 'Error processing request. temp_path_name=' + str(temp_path_name),
             } ),
             content_type="application/json"
         )
--- models/ore/ore_xls_writer.py
+++ models/ore/ore_xls_writer.py
@@ -1,10 +1,8 @@
-import StringIO
 import logging
 import os
 import shutil
 import string
 import random
-import sys
 
 __author__ = 'jflaisha'
 
@@ -258,6 +256,7 @@ class OreXls(object):
 
     def crop_target_sheet_user(self):
         # TODO: This only works for 1 crop chosen.  Multiple crops will be implemented later.
+        # TODO: 'null' values need to be changed to actual values
         crop_target_tab = ore_xls_formatter.OreXlsCropTarget(
             #  crop, group_no, group_name, subgroup_no, subgroup_name, crop_target
             self.ore_obj.input['exp_crop'],

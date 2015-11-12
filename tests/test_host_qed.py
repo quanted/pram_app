e14778c28c3d76bc9ef8f4aef1031195f4caba60
Laniak <laniak.gerry@epa.gov>
2015-11-12 16:46:55 -0500

    pip nose and qedinternal

--- tests/test_host_qed.py
+++ tests/test_host_qed.py
@@ -4,20 +4,34 @@ import numpy.testing as npt
 
 test = {}
 
-rooturl = ["http://qed.epa.gov/ubertool/"]
+rooturl_qed = ["http://qed.epa.gov/ubertool/"]
+rooturl_qedinternal = ["http://qedinternal.epa.gov/ubertool/"]
 models = ["sip/", "stir/", "rice/", "terrplant/"]
 pages = ["", "description"]
 
-model_pages = [r + m + p for r in rooturl for m in models for p in pages]
-print(model_pages)
+model_pages_qed = [r + m + p for r in rooturl_qed for m in models for p in pages]
+print(model_pages_qed)
+
+model_pages_qedinternal = [r + m + p for r in rooturl_qedinternal for m in models for p in pages]
+print(model_pages_qedinternal)
 
 class TestSipClient(unittest.TestCase):
     def setup(self):
         pass
 
-    def test_description(self):
+    def test_qed(self):
         try:
-            response = [requests.get(m).status_code for m in model_pages]
+            response = [requests.get(m).status_code for m in model_pages_qed]
+            #print(response.status_code)
+            #npt.assert_array_equal(3, 3, '', True)
+            npt.assert_array_equal(response, 200, '200 error', True)
+        finally:
+            pass
+        return
+
+    def test_qedinternal(self):
+        try:
+            response = [requests.get(m).status_code for m in model_pages_qedinternal]
             #print(response.status_code)
             #npt.assert_array_equal(3, 3, '', True)
             npt.assert_array_equal(response, 200, '200 error', True)

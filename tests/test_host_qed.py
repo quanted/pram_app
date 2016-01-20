17b2a25f392ba95848f4f409f8b4684ec909cf58
Laniak <laniak.gerry@epa.gov>
2016-01-20 16:53:28 -0500

    3 unittests passing for remote page checking

--- tests/test_host_qed.py
+++ tests/test_host_qed.py
@@ -13,7 +13,7 @@ import mechanize
 test = {}
 
 servers = ["http://qed.epa.gov/ubertool/", "http://qedinternal.epa.gov/ubertool/",
-           "http://134.67.114.3/ubertool/", "http://134.67.114.1/"]
+           "http://134.67.114.3/ubertool/", "http://134.67.114.1/ubertool/"]
 #models = ["sip/", "stir/", "rice/", "terrplant/", "trex2/", "therps/", "iec/",
 #          "agdrift/", "agdrift_trex/", "agdrift_therps/", "earthworm/",
 #          "rice/", "kabam/", "pfam/", "sam/"]
@@ -46,40 +46,80 @@ class TestQEDHost(unittest.TestCase):
         try:
             response = [requests.get(m) for m in redirect_model_pages]
             boo302 = [False] * len(response)
+            boocheck = [True] * len(response)
             urllist_302s = [""] * len(response)
             for i in range(len(response)):
                 for resp in response[i].history:
                     if resp.status_code == 302:
                         boo302[i] = True
                         urllist_302s[i] = resp.url
-            npt.assert_array_equal(boo302, False, '302 error', True)
+            npt.assert_array_equal(boo302, boocheck, '302 error', True)
         finally:
             pass
         return
 
-    def test_qed_authenticate(self):
+    def test_qed_authenticate_input(self):
         try:
             for m in redirect_model_pages :
+                #step 1: login and authenticate
                 br = mechanize.Browser()
-                br.open(m)
-                br.geturl()
+                response = br.open(m)
+                expected_page = redirect_servers[0] + models[0] + pages[2]
+                # try and extract list of forms from page
+                for form in br.forms():
+                   print '%r %r %s' % (form.name, form.attrs.get('id'), form.action)
                 br.select_form(name="auth")
                 br["username"] = "betatester"
                 br["password"] = "ubertool"
                 response2 = br.submit()
                 response2.get_data()
+                # Verify we have successfully logged in and are now on input page
+                current_page = br.geturl()
+                npt.assert_equal(expected_page, current_page,'Login Failed', True)
         finally:
             pass
         return
+
+    def test_qed_authenticate_output(self):
+        try:
+            for m in redirect_model_pages :
+                #step 1: login and authenticate
+                br = mechanize.Browser()
+                response = br.open(m)
+                expected_page = redirect_servers[0] + models[0] + pages[2]
+                # try and extract list of forms from page
+                for form in br.forms():
+                   print '%r %r %s' % (form.name, form.attrs.get('id'), form.action)
+                br.select_form(name="auth")
+                br["username"] = "betatester"
+                br["password"] = "ubertool"
+                response2 = br.submit()
+                response2.get_data()
+                # Verify we have successfully logged in and are now on input page
+                current_page = br.geturl()
+                npt.assert_equal(expected_page, current_page,'Login Failed', True)
+                # Step 2: Select input form (it will have default data in it  -  we just want to run with that for now)
+                br.form = list(br.forms())[0] # syntax for selecting form when form is unnamed
+                response3 = br.submit()  # use mechanize to post input data
+                #Verify we have successfully posted input data and that we have arrived at the output page
+                current_page = br.geturl()
+                expected_page = redirect_servers[0] + models[0] + "output"
+                npt.assert_equal(expected_page, current_page,'Submittal of Input Failed', True)
+
+        finally:
+            pass
+        return
+
 
     def test_qed_404(self):
         try:
             response = [requests.get(m) for m in model_pages]
             urllist_404s = [""] * len(response)
+            boo_check = [False] * len(response)
             soup_content = [BeautifulSoup(r.content, "html.parser") for r in response]
             soup_N404s = [len(s.findAll('img',src='/static/images/404error.png')) for s in soup_content]
             boo404 = [s>0 for s in soup_N404s]
-            npt.assert_array_equal(boo404, False, '404 error', True)
+            npt.assert_array_equal(boo404, boo_check, '404 error', True)
         finally:
             pass
         return

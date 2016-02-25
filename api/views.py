76c7975221c1ed3ae559f70e0d6923d92312fc16
Jon Flash <funkswing@gmail.com>
2016-02-25 17:08:57 -0500

    [API Docs] Updating API Styling

--- api/views.py
+++ api/views.py
@@ -14,6 +14,7 @@ def api_docs_view(request):
 
 
 def api_docs_json(request):
+    # TODO: Change to: /ubertool/api/spec
     url = os.environ['UBERTOOL_REST_SERVER'] + '/api/spec'
     api_json = requests.get(url)
 

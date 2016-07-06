cf1c5fa19b5f3d946bf62b8c916729c8bc4a31a9
Jon Flash <funkswing@gmail.com>
2016-07-06 17:29:48 -0400

    Fixed HWBI breaking ubertool links_left

--- models/hwbi/views_hwbi/links_left.py
+++ models/hwbi/views_hwbi/links_left.py
@@ -29,7 +29,7 @@ def ordered_list(model=None, page=None):
         # ),
     ])
 
-    return render_to_string('03ubertext_links_left_drupal.html', {
+    return render_to_string('hwbi/03ubertext_links_left_drupal.html', {
         'LINK_DICT': link_dict,
         'MODEL': model,
         'PAGE': page

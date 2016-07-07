73a1681e46121abfb2b4108871e49eb434c7e098
AIgnatius <arignatius@gmail.com>
2016-07-07 09:54:04 -0400

    update to Kurt's json

--- models/hwbi/static/js/slider.js
+++ models/hwbi/static/js/slider.js
@@ -15,28 +15,28 @@ function drawSliders(data, index) {
     //set scale for slider. domain is input min max. range is slider translated min max (same)
     var x = d3.scale.linear()
         .domain((function () {
-            if (data.serviceName == "capitalInvestment") { return [56.31683197, 61.75389692]; }
-            if (data.serviceName == "consumption") { return [47.42882423, 54.04916975]; }
-            if (data.serviceName == "employment") { return [33.13814798, 72.57176231]; }
-            if (data.serviceName == "finance") { return [31.42250002, 61.56578545]; }
-            if (data.serviceName == "innovation") { return [25.89488723, 65.3289721]; }
-            if (data.serviceName == "production") { return [45.11697104, 51.67193166]; }
-            if (data.serviceName == "redistribution") { return [23.51316313, 68.92691912]; }
-            if (data.serviceName == "airQuality") { return [10, 90]; }
-            if (data.serviceName == "foodFiberAndFuel") { return [32.62908483, 48.49178319]; }
-            if (data.serviceName == "greenspace") { return [36.11207908, 62.03906984]; }
-            if (data.serviceName == "waterQuality") { return [15.95637509, 88.22033237]; }
-            if (data.serviceName == "waterQuantity") { return [21.70976841, 72.83447998]; }
-            if (data.serviceName == "activism") { return [25.85945275, 73.66346154]; }
-            if (data.serviceName == "communication") { return [33.10020486, 68.98955269]; }
-            if (data.serviceName == "communityAndFaith") { return [12.21375305,	90]; }
-            if (data.serviceName == "education") { return [33.24429069,	56.47803694]; }
-            if (data.serviceName == "emergencyPreparedness") { return [19.78920564, 76.07510118]; }
-            if (data.serviceName == "familyServices") { return [42.66833596, 73.35259094]; }
-            if (data.serviceName == "healthcare") { return [29.63020433, 62.25876617]; }
-            if (data.serviceName == "justice") { return [31.22560175, 71.78167536]; }
-            if (data.serviceName == "labor") { return [36.52332879, 53.29715035]; }
-            if (data.serviceName == "publicWorks") { return [33.53645478, 66.4893089]; }
+            if (data.name == "capitalInvestment") { return [56.31683197, 61.75389692]; }
+            if (data.name == "consumption") { return [47.42882423, 54.04916975]; }
+            if (data.name == "employment") { return [33.13814798, 72.57176231]; }
+            if (data.name == "finance") { return [31.42250002, 61.56578545]; }
+            if (data.name == "innovation") { return [25.89488723, 65.3289721]; }
+            if (data.name == "production") { return [45.11697104, 51.67193166]; }
+            if (data.name == "redistribution") { return [23.51316313, 68.92691912]; }
+            if (data.name == "airQuality") { return [10, 90]; }
+            if (data.name == "foodFiberAndFuel") { return [32.62908483, 48.49178319]; }
+            if (data.name == "greenspace") { return [36.11207908, 62.03906984]; }
+            if (data.name == "waterQuality") { return [15.95637509, 88.22033237]; }
+            if (data.name == "waterQuantity") { return [21.70976841, 72.83447998]; }
+            if (data.name == "activism") { return [25.85945275, 73.66346154]; }
+            if (data.name == "communication") { return [33.10020486, 68.98955269]; }
+            if (data.name == "communityAndFaith") { return [12.21375305,	90]; }
+            if (data.name == "education") { return [33.24429069,	56.47803694]; }
+            if (data.name == "emergencyPreparedness") { return [19.78920564, 76.07510118]; }
+            if (data.name == "familyServices") { return [42.66833596, 73.35259094]; }
+            if (data.name == "healthcare") { return [29.63020433, 62.25876617]; }
+            if (data.name == "justice") { return [31.22560175, 71.78167536]; }
+            if (data.name == "labor") { return [36.52332879, 53.29715035]; }
+            if (data.name == "publicWorks") { return [33.53645478, 66.4893089]; }
         })()
     )
     .range([1, 99])
@@ -78,7 +78,7 @@ function drawSliders(data, index) {
         //set scale of brush to match d3 calculated scale
         .x(x)
         //set the input value using imported data Score value
-        .extent([data.score, data.score])
+        .extent([data.Score, data.Score])
         //set listener where on mousemove = function brushed
         .on("brush", brushed);
 
@@ -114,7 +114,7 @@ function drawSliders(data, index) {
     handle.append('text')
         .attr("class", "cirTex"+index)
         //assign text using data score and convert to whole number
-        .text(data.score)
+        .text(data.Score)
         .attr("transform", "translate(" + (-8) + " ," + 29 + ")");
 
     //for slider variable, call brush variable's "brushed" event property (mousemove)
@@ -142,7 +142,7 @@ function drawSliders(data, index) {
         //round text value down
         handle.select("text").text(Math.floor(value));
         
-        dragVal.services[index].score = value;
+        dragVal.services[index].Score = value;
     }
 }
 
@@ -153,43 +153,43 @@ function useServiceValues() {
 
     var postData = {
         "scores" : {
-            "capitalInvestment": dragVal.services[0].score,
-            "consumption": dragVal.services[1].score,
-            "employment": dragVal.services[2].score,
-            "finance": dragVal.services[3].score,
-            "innovation": dragVal.services[4].score,
-            "production": dragVal.services[5].score,
-            "redistribution": dragVal.services[6].score,
-            "airQuality": dragVal.services[7].score,
-            "foodFiberAndFuel": dragVal.services[8].score,
-            "greenspace": dragVal.services[9].score,
-            "waterQuality": dragVal.services[10].score,
-            "waterQuantity": dragVal.services[11].score,
-            "activism": dragVal.services[12].score,
-            "communication": dragVal.services[13].score,
-            "communityAndFaith": dragVal.services[14].score,
-            "education": dragVal.services[15].score,
-            "emergencyPreparedness": dragVal.services[16].score,
-            "familyServices": dragVal.services[17].score,
-            "healthcare": dragVal.services[18].score,
-            "justice": dragVal.services[19].score,
-            "labor": dragVal.services[20].score,
-            "publicWorks": dragVal.services[21].score
+            "capitalInvestment": dragVal.services[0].Score,
+            "consumption": dragVal.services[1].Score,
+            "employment": dragVal.services[2].Score,
+            "finance": dragVal.services[3].Score,
+            "innovation": dragVal.services[4].Score,
+            "production": dragVal.services[5].Score,
+            "redistribution": dragVal.services[6].Score,
+            "airQuality": dragVal.services[7].Score,
+            "foodFiberAndFuel": dragVal.services[8].Score,
+            "greenspace": dragVal.services[9].Score,
+            "waterQuality": dragVal.services[10].Score,
+            "waterQuantity": dragVal.services[11].Score,
+            "activism": dragVal.services[12].Score,
+            "communication": dragVal.services[13].Score,
+            "communityAndFaith": dragVal.services[14].Score,
+            "education": dragVal.services[15].Score,
+            "emergencyPreparedness": dragVal.services[16].Score,
+            "familyServices": dragVal.services[17].Score,
+            "healthcare": dragVal.services[18].Score,
+            "justice": dragVal.services[19].Score,
+            "labor": dragVal.services[20].Score,
+            "publicWorks": dragVal.services[21].Score
         },
         "domainWeights" : {
-            "ConnectionToNature": dragVal.domains[0].weight,
-            "CulturalFulfillment": dragVal.domains[1].weight,
-            "Education": dragVal.domains[2].weight,
-            "Health": dragVal.domains[3].weight,
-            "LeisureTime": dragVal.domains[4].weight,
-            "LivingStandards": dragVal.domains[5].weight,
-            "SafetyAndSecurity": dragVal.domains[6].weight,
-            "SocialCohesion": dragVal.domains[7].weight
+            "connectionToNature": dragVal.domains[0].weight,
+            "culturalFulfillment": dragVal.domains[1].weight,
+            "education": dragVal.domains[2].weight,
+            "health": dragVal.domains[3].weight,
+            "leisureTime": dragVal.domains[4].weight,
+            "livingStandards": dragVal.domains[5].weight,
+            "safetyAndSecurity": dragVal.domains[6].weight,
+            "socialCohesion": dragVal.domains[7].weight
         }
     };
 
     $.post(
-        '/rest/hwbi/calc',                      // url
+        '/hwbi/rest/hwbi/calc/run',                      // url
         JSON.stringify(postData),                               // data (as JS object)
         function(data) {                        // success (callback) function
             updateDomainScores(data.outputs.domains);
@@ -204,14 +204,14 @@ function useServiceValues() {
 
 //function to update RIV domain weight values
 function updateRIVWeights(domains) {
-    $('#connectionToNatureDomainWeight').val(domains[0].weight);
-    $('#culturalFulfillmentDomainWeight').val(domains[1].weight);
-    $('#educationDomainWeight').val(domains[2].weight);
-    $('#healthDomainWeight').val(domains[3].weight);
-    $('#leisureTimeDomainWeight').val(domains[4].weight);
-    $('#livingStandardsDomainWeight').val(domains[5].weight);
-    $('#safetyAndSecurityDomainWeight').val(domains[6].weight);
-    $('#socialCohesionDomainWeight').val(domains[7].weight);
+    $('#connectionToNature').val(domains[0].weight);
+    $('#culturalFulfillment').val(domains[1].weight);
+    $('#education').val(domains[2].weight);
+    $('#health').val(domains[3].weight);
+    $('#leisureTime').val(domains[4].weight);
+    $('#livingStandards').val(domains[5].weight);
+    $('#safetyAndSecurity').val(domains[6].weight);
+    $('#socialCohesion').val(domains[7].weight);
 }
 
 
@@ -228,28 +228,28 @@ function updateSliders(data, index) {
     //set scale for slider. domain is input min max. range is slider translated min max (same)
     var x = d3.scale.linear()
         .domain((function () {
-            if (data.serviceName == "capitalInvestment") { return [56.31683197, 61.75389692]; }
-            if (data.serviceName == "consumption") { return [47.42882423, 54.04916975]; }
-            if (data.serviceName == "employment") { return [33.13814798, 72.57176231]; }
-            if (data.serviceName == "finance") { return [31.42250002, 61.56578545]; }
-            if (data.serviceName == "innovation") { return [25.89488723, 65.3289721]; }
-            if (data.serviceName == "production") { return [45.11697104, 51.67193166]; }
-            if (data.serviceName == "redistribution") { return [23.51316313, 68.92691912]; }
-            if (data.serviceName == "airQuality") { return [10, 90]; }
-            if (data.serviceName == "foodFiberAndFuel") { return [32.62908483, 48.49178319]; }
-            if (data.serviceName == "greenspace") { return [36.11207908, 62.03906984]; }
-            if (data.serviceName == "waterQuality") { return [15.95637509, 88.22033237]; }
-            if (data.serviceName == "waterQuantity") { return [21.70976841, 72.83447998]; }
-            if (data.serviceName == "activism") { return [25.85945275, 73.66346154]; }
-            if (data.serviceName == "communication") { return [33.10020486, 68.98955269]; }
-            if (data.serviceName == "communityAndFaith") { return [12.21375305,	90]; }
-            if (data.serviceName == "education") { return [33.24429069,	56.47803694]; }
-            if (data.serviceName == "emergencyPreparedness") { return [19.78920564, 76.07510118]; }
-            if (data.serviceName == "familyServices") { return [42.66833596, 73.35259094]; }
-            if (data.serviceName == "healthcare") { return [29.63020433, 62.25876617]; }
-            if (data.serviceName == "justice") { return [31.22560175, 71.78167536]; }
-            if (data.serviceName == "labor") { return [36.52332879, 53.29715035]; }
-            if (data.serviceName == "publicWorks") { return [33.53645478, 66.4893089]; }
+            if (data.name == "capitalInvestment") { return [56.31683197, 61.75389692]; }
+            if (data.name == "consumption") { return [47.42882423, 54.04916975]; }
+            if (data.name == "employment") { return [33.13814798, 72.57176231]; }
+            if (data.name == "finance") { return [31.42250002, 61.56578545]; }
+            if (data.name == "innovation") { return [25.89488723, 65.3289721]; }
+            if (data.name == "production") { return [45.11697104, 51.67193166]; }
+            if (data.name == "redistribution") { return [23.51316313, 68.92691912]; }
+            if (data.name == "airQuality") { return [10, 90]; }
+            if (data.name == "foodFiberAndFuel") { return [32.62908483, 48.49178319]; }
+            if (data.name == "greenspace") { return [36.11207908, 62.03906984]; }
+            if (data.name == "waterQuality") { return [15.95637509, 88.22033237]; }
+            if (data.name == "waterQuantity") { return [21.70976841, 72.83447998]; }
+            if (data.name == "activism") { return [25.85945275, 73.66346154]; }
+            if (data.name == "communication") { return [33.10020486, 68.98955269]; }
+            if (data.name == "communityAndFaith") { return [12.21375305,	90]; }
+            if (data.name == "education") { return [33.24429069,	56.47803694]; }
+            if (data.name == "emergencyPreparedness") { return [19.78920564, 76.07510118]; }
+            if (data.name == "familyServices") { return [42.66833596, 73.35259094]; }
+            if (data.name == "healthcare") { return [29.63020433, 62.25876617]; }
+            if (data.name == "justice") { return [31.22560175, 71.78167536]; }
+            if (data.name == "labor") { return [36.52332879, 53.29715035]; }
+            if (data.name == "publicWorks") { return [33.53645478, 66.4893089]; }
         })()
     )
     .range([1, 99])
@@ -261,7 +261,7 @@ function updateSliders(data, index) {
         //set scale of brush to match d3 calculated scale
         .x(x)
         //set the input value using imported data Score value
-        .extent([data.score, data.score])
+        .extent([data.Score, data.Score])
         //set listener where on mousemove = function brushed
         .on("brush", brushed);
 
@@ -285,7 +285,7 @@ function updateSliders(data, index) {
     //select text to the handle
     d3.select("text.cirTex" + [index])
         //assign text using data score and convert to whole number
-        .text(data.score)
+        .text(data.Score)
         .attr("transform", "translate(" + (-8) + " ," + 29 + ")");
 
     //for slider variable, call brush variable's "brushed" event property (mousemove)

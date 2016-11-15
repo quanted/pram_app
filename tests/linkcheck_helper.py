cc49bb195dd1d37c5c346c5e652e87ffe3cad765
Laniak <laniak.gerry@epa.gov>
2016-11-15 16:26:37 -0500

    orphaned commit

--- tests/linkcheck_helper.py
+++ tests/linkcheck_helper.py
@@ -30,13 +30,14 @@ def status_chk(url_list):
     return status
 
 def build_table(list1, list2):
-    # function builds a two column table containing url's and status codes
+    # function builds a two column table from two lists (e.g., url's and status codes)
     report = [""] * len(list1)
     for idx, item in enumerate(list1):
         report[idx] = [list1[idx], list2[idx]]
     return report
 
 def write_report(test_name, assert_error, col1, col2):
+    # method prints results of tests (two paths depending on whether test passed or failed)
     if assert_error:
         print test_name + "Test failed for one or more instances"
         report = build_table(col1, col2)

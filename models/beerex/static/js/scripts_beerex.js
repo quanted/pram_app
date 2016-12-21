07489b4b1505d8e5829046f7be54b236593ea0cd
Carmen Kuan <ashleyc.kuan@gmail.com>
2016-12-21 14:25:32 -0500

    jquery show hide input fields

--- models/beerex/static/js/scripts_beerex.js
+++ models/beerex/static/js/scripts_beerex.js
@@ -2,40 +2,39 @@ $(document).ready(function() {
   
     // Add 'dividers' between table sections
     $('#id_log_kow, #id_adult_contact_ld50, #id_empirical_residue, #id_lw1_jelly').closest('tr').before('<tr><td colspan="2"></td></tr>');
-    // Default 'readonly' inputs
-    $('#id_log_kow, #id_koc, #id_mass_tree_vegetation, #id_empirical_pollen, #id_empirical_nectar, #id_empirical_jelly').prop('readonly', true);
-
 
     // onChange functions
     $('#id_application_method').change(function() {
-        if ($(this).val() == "soil application"){
-            $('#id_log_kow').prop('readonly', true).val('350');
+
+        if ($(this).val() == "soil application") {
+            $('#id_log_kow').show();
+            $('#id_koc').show();
+        }
+        else {
+            $('#id_log_kow').hide();
+            $('#id_koc').hide();
+        }
+
+
+        if ($(this).val() == "tree trunk"){
+            $('#id_mass_tree_vegetation').show();
         }
         else{
-           $('#id_log_kow').prop('readonly', false);
+            $('#id_mass_tree_vegetation').hide();
         }
     });
 
 
-    $('#id_noael_species_tested_mammal').change(function() {
-        if ($(this).val() == "350"){
-            $('#id_noael_bodyweight_tested_mammal').prop('readonly', true).val('350');
+    $('#id_empirical_residue').change(function() {
+        if ($(this).val() == "yes"){
+            $('#id_empirical_pollen').show();
+            $('#id_empirical_nectar').show();
+            $('#id_empirical_jelly').show();
         }
         else{
-           $('#id_noael_bodyweight_tested_mammal').prop('readonly', false);
-        }
-    });
-
-
-    $('#id_species_tested_bird').change(function() {
-        if ($(this).val() == "178"){
-            $('#id_bodyweight_tested_bird').prop('readonly', true).val('178');
-        }
-        else if ($(this).val() == "1580"){
-            $('#id_bodyweight_tested_bird').prop('readonly', true).val('1580');
-        }
-        else{
-           $('#id_bodyweight_tested_bird').prop('readonly', false);
+            $('#id_empirical_pollen').hide();
+            $('#id_empirical_nectar').hide();
+            $('#id_empirical_jelly').hide();
         }
     });
 

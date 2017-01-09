3d8941e813b0cb9cc6646ec80f758c0b81f57d37
Tom Purucker <purucker.tom@gmail.com>
2017-01-09 00:30:41 -0500

    test page for 2017 template

--- views/landing.py
+++ views/landing.py
@@ -15,19 +15,23 @@ def eco_landing_page_new(request):
     text_file2 = open(os.path.join(os.environ['PROJECT_PATH'], 'ubertool_app/views/landing_text.txt'), 'r')
     xx = text_file2.read()
 
-    html = render_to_string('01epa_drupal_header.html', {})
+    #html = render_to_string('01epa_drupal_header.html', {})
+    html = render_to_string('01epa_drupal_header.html', {
+        'SITE_SKIN': os.environ['SITE_SKIN'],
+        'TITLE': u"\u00FCbertool"
+    })
     #html = render_to_string('01uberheader_main_drupal.html', {
     #    'SITE_SKIN': os.environ['SITE_SKIN'],
     #    'TITLE': u"\u00FCbertool"
     #})
     html += render_to_string('02epa_drupal_header_bluestripe.html', {})
     html += render_to_string('04uber_drupal_frog_intro.html', {})
-    html += links_left.ordered_list()
-    html += render_to_string('04ubertext_start_index_drupal.html', {
+    html += links_left.ordered_list() #fills out 05ubertext_links_left_drupal.html
+    html += render_to_string('06ubertext_start_index_drupal.html', {
         'TITLE': u"\u00FCbertool",
         'TEXT_PARAGRAPH': xx
     })
-    html += render_to_string('04ubertext_end_drupal.html', {})
+    html += render_to_string('07ubertext_end_drupal.html', {})
 
     #html += render_to_string('06uberfooter.html', {})
     html += render_to_string('10epa_drupal_footer.html', {})

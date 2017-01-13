c301d71aa35fcd5fc230e585698b518efd94854f
sumathys <sumathy.sinnathamby@gmail.com>
2017-01-13 09:51:27 -0500

    Reference links updated

--- models/sam/views.py
+++ models/sam/views.py
@@ -78,5 +78,37 @@ history = '<p>User History</p>'
 references = '<p><ul class=\"bullet\">' \
              '<li><a href=\"http://sam-manual.s3-website-us-east-1.amazonaws.com/SAM_ubertool_user_manual_21Apr2015.pdf\">SAM User Manual' \
              '4/21/2015</a>' \
+             '<li><a href=\"https://github.com/quanted/static_qed/blob/master/docs/SAM_Developmental_Manual\">' \
+             'DEVELOPMENT OF A SPATIAL AQUATIC MODEL (SAM) FOR PESTICIDE ASSESSMENTS</a></li>' \
              '</ul>' \
-             '</p>'
+             '</p>'\
+             'Presentations/posters/abstracts/reports on the subject of EPA\'s ecological risk assessment ' \
+             'models, including SAM.' \
+             '</p>' \
+             '<ul class=\"bullet\">' \
+             '<li><a href=\"http://www.cdpr.ca.gov/docs/emon/surfwtr/presentations/presentation_110.pdf\">' \
+             'Spatially distributed modeling for pesticide assessments ' \
+             'in surface water, and USEPA Spatial Aquatic Model</a></li>' \
+             '<li><a href=\"http://www.agrodiv.org/documents/denver11/Evaluating%20Agrochemical%20Aquatic%20Exposure/19_Parker_Ronald.pdf\">' \
+             'Exploring Approaches to Pesticide Aquatic Ecological Exposure Assessment:Issues in Evaluating Risk Across the National Landscape , Ron Parker (USEPA)</a></li>' \
+             '<li><a href=\"http://www.complianceservices.com/wp-content/uploads/2013/06/Next-Steps-in-Endangered-Species-Risk-Assessments.pdf\">' \
+             'Next Steps in Endangered Species Risk Assessments, Cliff Habig (Compliance Services International)</a></li>' \
+             '<li><a href=\"https://nctc.fws.gov/resources/course-resources/pesticides/2011Presentations/Tab%2013%20%20Pesticide%20Risk%20Assessment.pdf\">' \
+             'EPA Pesticide Ecological Risk Assessment Methods, Fish and Wildlife Service Training</a></li>' \
+             '<li><a href=\"http://www.calcitrusquality.org/wp-content/uploads/2011/07/Prometryncasestudy.pdf\">' \
+             'Minor Crop Farm Alliance Endangered Species Assessment Workshop- Prometryn Case Study</a></li>' \
+             '<li><a href=\"https://www.epa.gov/risk/risk-tools-and-databases\">' \
+             'Ecological Risk Assessment and Improving Evaluation Tools, Don Brady (USEPA)</a></li>' \
+             '<li><a href=\"www.cdpr.ca.gov/docs/enforce/drftinit/confs/2001/esterly.ppt">AgDrift Training, Dave Easterly, Environmental Focus, Inc\"' \
+             '<li><a href=\"http://tpsalliance.org/pdf/conference/2008/2B_Hewitt_Droplet%20Size%20Calculators.pdf\">' \
+             'Atomization Models for Actual Tank Mixes, Andrew Hewitt, University of Queensland</a></li>' \
+             '<li><a href=\"http://www.nap.edu/catalog.php?record_id=18344\">' \
+             'Assessing Risks to Endangered and Threatened Species From Pesticides (National Academy of Sciences)</a>' \
+             '</li></ul>' \
+             '<p>'\
+             'Peer reviewed literature on SAM:' \
+             '</p>' \
+             '<ul class=\"bullet\">' \
+             '<li><a href=\"https://github.com/quanted/static_qed/blob/master/docs/SAM_ACS_Hoogeweg.pdf\">' \
+             'Development of a spatial-temporal cooccurrence index to evaluate relative pesticide risks to threatened and endangered species</a></li>' \
+             '</ul>'

e1810bcb72406cd9a5910cc5cdfae2cb8a413c85
Tom Purucker <purucker.tom@gmail.com>
2017-02-16 22:48:09 +0000

    agdrift parameter name changes

--- models/agdrift/agdrift_parameters.py
+++ models/agdrift/agdrift_parameters.py
@@ -7,80 +7,131 @@ from django.utils.safestring import mark_safe
 from django.core import validators
 from ubertool_app.models.forms import validation
 
+Ecosystem_type_CHOICES=(('0','Make a selection'),
+                        ('Aquatic Assessment','Aquatic Assessment'),
+                        ('Terrestrial Assessment', 'Terrestrial Assessment'))
+Application_method_CHOICES=(('0','Make a selection'),
+                            ('Tier I Aerial','Tier I Aerial'),
+                            ('Tier I Ground','Tier I Ground'),
+                            ('Tier I Orchard/Airblast','Tier I Orchard/Airblast'))
+Drop_size_distribution_CHOICES=(('0','Make a selection'),
+                                ('Very fine to fine','Very fine to fine'),
+                                ('Fine to Medium','Fine to Medium'),
+                                ('Medium to Coarse','Medium to Coarse'),
+                                ('Coarse to Very Coarse','Coarse to Very Coarse'))
+Boom_height_CHOICES=(('0','Make a selection'),
+                     ('Low','Low'),
+                     ('High','High'))
 
-Application_method_CHOICES=(('0','Make a selection'),('Aerial','Tier I Aerial'),('Ground','Tier I Ground'),('Orchard/Airblast','Tier I Orchard/Airblast'))
-Drop_size_distribution_CHOICES=(('0','Make a selection'),('Fine','Very fine to fine'),('Medium','Fine to Medium'),('Coarse','Medium to Coarse'), ('Very Coarse','Coarse to Very Coarse'))
-Boom_height_CHOICES=(('0','Make a selection'),('Low','Low'),('High','High'))
-Ecosystem_type_CHOICES=(('0','Make a selection'),('EPA Pond','Aquatic Assessment'),('Terrestrial', 'Terrestrial Assessment')) #Aquatic Assessment
+Orchard_CHOICES=(('0','Make a selection'),
+                 ('Normal','Normal (Stone and Pome Fruit Vineyard)'),
+                 ('Dense','Dense (Citrus, tall trees)'),
+                 ('Sparse', 'Sparse (Young, dormant)'),
+                 ('Vineyard', 'Vineyard'),
+                 ('Orchard','Orchard'))
+Terrestrial_type_CHOICES=(('0','Make a selection'),
+                          ('EPA Defined Terrestrial','EPA Defined Terrestrial'),
+                          ('User Defined Terrestrial','User Defined Terrestrial'))
+Aquatic_type_CHOICES=(('0','Make a selection'),
+                      ('EPA Defined Pond','EPA Defined Pond'),
+                      ('EPA Defined Wetland', 'EPA Defined Wetland'))
+Calculation_input_CHOICES=(('0','Make a selection'),
+                           ('Distance','Distance to waterbody or field'),
+                           ('Fraction','Fraction of applied'),
+                           ('Initial Average Deposition (g/ha)','Initial Average Deposition (g/ha)'),
+                           ('Initial Average Deposition (lb/ac)', 'Initial Average Deposition (lb/ac)'),
+                           ('Initial Average Concentration (ng/L)', 'Initial Average Concentration (ng/L)'),
+                           ('Initial Average Deposiion (mg/cm2)','Initial Average Deposition (mg/cm2)'))
+SELECT_VERSION = (('1.0','1.0'),)
+#Aquatic Assessment
 #Waterbody_type_CHOICES=(('','Make a selection'),('EPA Pond','EPA Pond'),('Lake', 'Lake'), ('Watercourse', 'Watercourse')) #Aquatic Assessment
 #Orchard_CHOICES=(('','Make a selection'),('Vineyard in leaf','Vineyard in leaf'),('Orchard or dormant vineyard','Orchard or dormant vineyard'))
 #Waterbody_type_CHOICES=(('','Make a selection'),('EPA Pond','EPA Pond'),('Lake', 'Lake'), ('Watercourse', 'Watercourse')) #Aquatic Assessment
-Orchard_CHOICES=(('0','Make a selection'),('Normal','Normal (Stone and Pome Fruit Vineyard)'),('Dense','Dense (Citrus, tall trees)'), ('Sparse', 'Sparse (Young, dormant)'),('Vineyard', 'Vineyard'),('Orchard','Orchard'))
-Aquatic_type_CHOICES=(('0','Make a selection'),('1','EPA Defined Pond'),('2', 'EPA Defined Wetland'))
-Calculation_input_CHOICES=(('0','Make a selection'), ('Distance','Distance to waterbody or field'), ('Fraction','Fraction of applied'),('Initial Average Deposition (g/ha)','Initial Average Deposition (g/ha)'),('Initial Average Deposition (lb/ac)', 'Initial Average Deposition (lb/ac)'),('Initial Average Concentration (ng/L)', 'Initial Average Concentration (ng/L)'), ('Initial Average Deposiion (mg/cm2)','Initial Average Deposition (mg/cm2)'))
-SELECT_VERSION = (('1.0','1.0'),)
 
 class AgdriftInp(forms.Form):
-#    waterbody_type = forms.ChoiceField(
-            # label='Water body type', choices=Waterbody_type_CHOICES,initial='Make a selection')
+
     version = forms.ChoiceField(
-            choices=SELECT_VERSION, 
-            label='Version',
-            initial='1.2.2')
+        choices=SELECT_VERSION,
+        label='Version',
+        initial='1.2.2')
     chemical_name = forms.CharField(
-            widget=forms.Textarea (attrs={'cols': 30, 'rows': 1}),
-            label='Chemical Name',
-            initial='Alachlor')
+        widget=forms.Textarea (attrs={'cols': 30, 'rows': 1}),
+        label='Chemical Name',
+        initial='Alachlor')
     pc_code = forms.CharField(
-            widget=forms.Textarea (attrs={'cols': 20, 'rows': 1}), 
-            label='PC Code',
-            initial='00')
-    application_rate = forms.FloatField(
-            label=mark_safe('Active rate (lb/ac)'),
-            initial='0.5',
-            validators=[validation.validate_greaterthan0])
-    application_method = forms.ChoiceField(
-            label='Application Method',
-            choices=Application_method_CHOICES,
-            initial='Make a selection',
-            validators=[validation.validate_choicefield])    
-    drop_size = forms.ChoiceField(
-            label='Drop Size Distribution',
-            choices=Drop_size_distribution_CHOICES,
-            initial='Medium',
-            validators=[validation.validate_choicefield])
+        widget=forms.Textarea (attrs={'cols': 20, 'rows': 1}),
+        label='PC Code',
+        initial='00')
     ecosystem_type = forms.ChoiceField(
-            label='Ecosystem type',
-            choices=Ecosystem_type_CHOICES,
-            initial='EPA Pond',
-            validators=[validation.validate_choicefield])
+        label='Ecosystem type',
+        choices=Ecosystem_type_CHOICES,
+        initial='Aquatic Assessment',
+        validators=[validation.validate_choicefield])
+    application_method = forms.ChoiceField(
+        label='Application Method',
+        choices=Application_method_CHOICES,
+        initial='Tier I Aerial',
+        validators=[validation.validate_choicefield])
+    aquatic_body_type = forms.ChoiceField(
+        label='Aquatic Assessment Type',
+        choices=Aquatic_type_CHOICES,
+        initial='EPA Defined Pond',
+        validators=[validation.validate_choicefield])
+    drop_size = forms.ChoiceField(
+        label='Drop Size Distribution',
+        choices=Drop_size_distribution_CHOICES,
+        initial='Medium to Coarse',
+        validators=[validation.validate_choicefield])
+    application_rate = forms.FloatField(
+        label=mark_safe('Active rate (lb/ac)'),
+        initial='0.5',
+        validators=[validation.validate_greaterthan0])
     boom_height = forms.ChoiceField(
-            label='Boom height',
-            choices=Boom_height_CHOICES,
-            initial='High',
-            validators=[validation.validate_choicefield])
-    orchard_type = forms.ChoiceField(
-            label='Orchard type',
-            choices=Orchard_CHOICES,
-            initial='Orchard',
-            validators=[validation.validate_choicefield])
+        label='Boom height',
+        choices=Boom_height_CHOICES,
+        initial='High',
+        validators=[validation.validate_choicefield])
+    airblast_type = forms.ChoiceField(
+        label='Orchard type',
+        choices=Orchard_CHOICES,
+        initial='Orchard',
+        validators=[validation.validate_choicefield])
+    calculation_input = forms.ChoiceField(
+        label = 'Toolbox Input Type',
+        choices=Calculation_input_CHOICES,
+        initial ='Distance',
+        validators=[validation.validate_choicefield])
+    downwind_distance = forms.FloatField(
+        label=mark_safe('Distance to water body or terrestrial point from edge of field (ft)'),
+        initial='225',
+        validators=[validation.validate_integer, validation.validate_greaterthan0])
+    user_pond_width = forms.FloatField(
+        label=mark_safe('Pond Width (ft)'),
+        initial='208.7',
+        validators=[validation.validate_greaterthan0])
+    user_pond_depth = forms.FloatField(
+        label=mark_safe('Pond depth (ft)'),
+        initial='6.56',
+        validators=[validation.validate_greaterthan0])
+    user_wetland_width = forms.FloatField(
+        label=mark_safe('Wetland Width (ft)'),
+        initial='0.4921',
+        validators=[validation.validate_greaterthan0])
+    user_wetland_depth = forms.FloatField(
+        label=mark_safe('Wetland depth (ft)'),
+        initial='208.7',
+        validators=[validation.validate_greaterthan0])
+    terrestrial_field_type = forms.ChoiceField(
+        label='Terrestrial Field type',
+        choices=Terrestrial_type_CHOICES,
+        initial='EPA Defined Terrestrial',
+        validators=[validation.validate_choicefield])
+    user_terrestrial_width = forms.FloatField(
+        label=mark_safe('Downwind width of Area (ft)'),
+        initial='208.7',
+        validators=[validation.validate_greaterthan0])
 #    extending_settings = forms.ChoiceField(
             # label='Optional settings', choices=Extended_settings_CHOICES, initial='Make a selection')
-
-    aquatic_type = forms.ChoiceField(
-            label='Aquatic Assessment Type',
-            choices=Aquatic_type_CHOICES,
-            initial='1',
-            validators=[validation.validate_choicefield])    
-    calculation_input = forms.ChoiceField(
-            label = 'Toolbox Input Type',
-            choices=Calculation_input_CHOICES,
-            initial ='Distance',
-            validators=[validation.validate_choicefield])
-    distance = forms.FloatField(
-            label=mark_safe('Distance to water body or terrestrial point from edge of field (ft)'),
-            initial='225',
-            validators=[validation.validate_integer, validation.validate_greaterthan0])
  #   init_avg_dep_foa = forms.FloatField(
             # label=mark_safe('Fraction of applied'))
  #   avg_depo_gha = forms.FloatField(
--- models/agdrift/agdrift_tables.py
+++ models/agdrift/agdrift_tables.py
@@ -66,8 +66,8 @@ def getdjtemplate():
 
 def gett1data(agdrift_obj):
     data = { 
-        "Parameter": ['Application method', 'Orchard type', 'Drop size', 'Ecosystem type', ],
-        "Value": [agdrift_obj.application_method, agdrift_obj.orchard_type, agdrift_obj.drop_size, agdrift_obj.ecosystem_type,],
+        "Parameter": ['Assessment type', 'Application method', 'Aquatic Body Type', 'Drop size', 'Scenario', ],
+        "Value": [agdrift_obj.ecosystem_type, agdrift_obj.application_method, agdrift_obj.aquatic_body_type, agdrift_obj.drop_size, agdrift_obj.out_sim_scenario_chk,],
     }
     return data
 
@@ -81,40 +81,10 @@ def gett2data(agdrift_obj):
     logger.info(vars(agdrift_obj))
     data = { 
         "Parameter": ['Spray drift fraction of applied', 'Initial Average Deposition (g/ha)', 'Initial Average Deposition (lb/ac)', 'Initial Average Concentration (ng/L)', 'Initial Average Deposition (mg/cm2)', 'Distance to Point or Waterbody (ft)',],
-        # "Value": ['%.3f' % agdrift_obj.out_init_avg_dep_foa,'%.3f' % agdrift_obj.out_avg_depo_gha,'%.3f' % agdrift_obj.out_avg_depo_lbac, '%.3f' % agdrift_obj.out_deposition_ngl, '%.3f' % agdrift_obj.out_deposition_mgcm,],
-        "Value": ['%.5f' % agdrift_obj.out_init_avg_dep_foa,'%.5f' % agdrift_obj.out_avg_depo_gha,'%.5f' % agdrift_obj.out_avg_depo_lbac, '%.5f' % agdrift_obj.out_deposition_ngl, '%.5f' % agdrift_obj.out_deposition_mgcm, '%.d' % int(agdrift_obj.distance),],
+        # "Value": ['%.3f' % agdrift_obj.out_init_avg_dep_foa,'%.3f' % agdrift_obj.out_avg_dep_gha,'%.3f' % agdrift_obj.out_avg_dep_lbac, '%.3f' % agdrift_obj.out_deposition_ngl, '%.3f' % agdrift_obj.out_avg_field_dep_mgcm,],
+        "Value": ['%.5f' % agdrift_obj.out_avg_dep_foa,'%.5f' % agdrift_obj.out_avg_dep_gha,'%.5f' % agdrift_obj.out_avg_dep_lbac, '%.5f' % agdrift_obj.out_avg_waterconc_ngl, '%.5f' % agdrift_obj.out_avg_field_dep_mgcm, '%.d' % int(agdrift_obj.downwind_distance),],
     }
     return data
-
-def gettsumdata(application_rate,distance):
-    data = { 
-        "Parameter": ['Application rate','Distance',],
-        "Mean": ['%.2e' % numpy.mean(application_rate),'%.2e' % numpy.mean(distance),],
-        "Std": ['%.2e' % numpy.std(application_rate),'%.2e' % numpy.std(distance),],
-        "Min": ['%.2e' % numpy.min(application_rate),'%.2e' % numpy.min(distance),],        
-        "Max": ['%.2e' % numpy.max(application_rate),'%.2e' % numpy.max(distance),], 
-        "Unit": ['lb/ac', 'm',],
-    }
-    return data
-
-def gettsumdata_out(out_init_avg_dep_foa,out_avg_depo_lbac,out_avg_depo_gha,out_deposition_ngl,out_deposition_mgcm,out_nasae,out_y,out_x,out_express_y):
-    data = { 
-        "Parameter": ['Average deposition','Deposition (lb/ac)', 'Deposition (g/ha)','Deposition (ng/L)','Deposition (mg/cm)','NASAE','Y','X','Express_Y'],                 
-        "Mean": ['%.2e' % numpy.mean(out_init_avg_dep_foa),'%.2e' % numpy.mean(out_avg_depo_lbac),'%.2e' % numpy.mean(out_avg_depo_gha),
-        '%.2e' % numpy.mean(out_deposition_ngl),'%.2e' % numpy.mean(out_deposition_mgcm),'%.2e' % numpy.mean(out_nasae),'%.2e' % numpy.mean(out_y),
-        '%.2e' % numpy.mean(out_x),'%.2e' % numpy.mean(out_express_y),],
-        "Std": ['%.2e' % numpy.std(out_init_avg_dep_foa),'%.2e' % numpy.std(out_avg_depo_lbac),'%.2e' % numpy.std(out_avg_depo_gha),
-        '%.2e' % numpy.std(out_deposition_ngl),'%.2e' % numpy.std(out_deposition_mgcm),'%.2e' % numpy.std(out_nasae),'%.2e' % numpy.std(out_y),
-        '%.2e' % numpy.std(out_x),'%.2e' % numpy.std(out_express_y),],
-        "Min": ['%.2e' % numpy.min(out_init_avg_dep_foa),'%.2e' % numpy.min(out_avg_depo_lbac),'%.2e' % numpy.min(out_avg_depo_gha),
-        '%.2e' % numpy.min(out_deposition_ngl),'%.2e' % numpy.min(out_deposition_mgcm),'%.2e' % numpy.min(out_nasae),'%.2e' % numpy.min(out_y),
-        '%.2e' % numpy.min(out_x),'%.2e' % numpy.min(out_express_y),],
-        "Max": ['%.2e' % numpy.max(out_init_avg_dep_foa),'%.2e' % numpy.max(out_avg_depo_lbac),'%.2e' % numpy.max(out_avg_depo_gha),
-        '%.2e' % numpy.max(out_deposition_ngl),'%.2e' % numpy.max(out_deposition_mgcm),'%.2e' % numpy.max(out_nasae),'%.2e' % numpy.max(out_y),
-        '%.2e' % numpy.max(out_x),'%.2e' % numpy.max(out_express_y),],   
-        "Unit": ['lb/ac', 'm','','','','',''],
-    }
-    return data        
 
 pvuheadings = getheaderpvu()
 pvrheadings = getheaderpvr()
@@ -126,8 +96,8 @@ tmpl = Template(djtemplate)
 def table_all(agdrift_obj):
     html_all = table_1(agdrift_obj)     
     html_all = html_all + table_2(agdrift_obj)
-    html_all = html_all + table_3(agdrift_obj)
-    html_all = html_all + render_to_string('agdrift-output-jqplot.html', {'chart_num':1}) #agdrift_obj.loop_indx
+    #html_all = html_all + table_3(agdrift_obj)
+    #html_all = html_all + render_to_string('agdrift-output-jqplot.html', {'chart_num':1}) #agdrift_obj.loop_indx
     return html_all
 
 def table_all_qaqc(agdrift_obj):
@@ -205,42 +175,3 @@ def table_3(agdrift_obj):
         #         </div>
         # """
         return html
-
-def table_sum_input(i,application_rate,distance):
-        #pre-table sum_input
-        html = """
-        <H3 class="out_1 collapsible" id="section1"><span></span>Batch Summary Statistics (Iterations=%s)</H3>
-        <div class="out_">
-            <H4 class="out_1 collapsible" id="section4"><span></span>Batch Inputs</H4>
-                <div class="out_ container_output">
-        """%(i-1)
-        #table sum_input
-        tsuminputdata = gettsumdata(application_rate,distance)
-        tsuminputrows = gethtmlrowsfromcols(tsuminputdata, sumheadings)
-        html = html + tmpl.render(Context(dict(data=tsuminputrows, headings=sumheadings)))
-        html = html + """
-        </div>
-        """
-        return html
-
-def table_sum_output(out_init_avg_dep_foa,out_avg_depo_lbac,out_avg_depo_gha,out_deposition_ngl,out_deposition_mgcm,out_nasae,out_y,out_x,out_express_y):
-
-        #pre-table sum_input
-        html = """
-            <H4 class="out_1 collapsible" id="section3"><span></span>Batch Outputs</H4>
-                <div class="out_ container_output">
-        """
-        #table sum_input
-        tsumoutputdata = gettsumdata_out(out_init_avg_dep_foa,out_avg_depo_lbac,out_avg_depo_gha,out_deposition_ngl,out_deposition_mgcm,out_nasae,out_y,out_x,out_express_y)
-        tsumoutputrows = gethtmlrowsfromcols(tsumoutputdata, sumheadings)
-        html = html + tmpl.render(Context(dict(data=tsumoutputrows, headings=sumheadings)))
-        html = html + """
-                </div>
-        </div>
-        <br>
-        """
-        return html   
-
-
-
-

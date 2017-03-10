69d6f62fbeb5ce12d1fcd73adf89f2e43bb1c8a9
Tom Purucker <purucker.tom@gmail.com>
2017-03-10 16:32:40 +0000

    testing pull requests 2

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    #this is no more
     url(r'^$', views.api_docs_view, name='api_docs_view'),
-    url(r'^spec/?$', views.api_docs_json)
+    url(r'^spec/?$', views.api_docs_json),
 ]

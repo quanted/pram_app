73b22fe2b529c222fe4199ba6cc2334af2fb9e11
Gerry Laniak <laniak.gerry@epa.gov>
2017-04-25 17:19:14 -0400

    front end agdrift parameter updates

--- models/agdrift/agdrift_parameters.py
+++ models/agdrift/agdrift_parameters.py
@@ -14,11 +14,14 @@ Application_method_CHOICES=(('0','Make a selection'),
                             ('Tier I Aerial','Tier I Aerial'),
                             ('Tier I Ground','Tier I Ground'),
                             ('Tier I Orchard/Airblast','Tier I Orchard/Airblast'))
-Drop_size_distribution_CHOICES=(('0','Make a selection'),
+Drop_size_distribution_aerial_CHOICES=(('0','Make a selection'),
                                 ('Very Fine to Fine','Very Fine to Fine'),
                                 ('Fine to Medium','Fine to Medium'),
                                 ('Medium to Coarse','Medium to Coarse'),
                                 ('Coarse to Very Coarse','Coarse to Very Coarse'))
+Drop_size_distribution_ground_CHOICES=(('0','Make a selection'),
+                                ('Very Fine to Fine','Very Fine to Fine'),
+                                ('Fine to Medium/Coarse','Fine to Medium/Coarse'))
 Boom_height_CHOICES=(('0','Make a selection'),
                      ('Low','Low'),
                      ('High','High'))
@@ -30,11 +33,14 @@ Orchard_CHOICES=(('0','Make a selection'),
                  ('Vineyard', 'Vineyard'),
                  ('Orchard','Orchard'))
 Terrestrial_type_CHOICES=(('0','Make a selection'),
-                          ('EPA Defined Terrestrial','EPA Defined Terrestrial'),
-                          ('User Defined Terrestrial','User Defined Terrestrial'))
+                          # ('EPA Defined Terrestrial','EPA Defined Terrestrial'),
+                          # ('User Defined Terrestrial','User Defined Terrestrial'))
+                          ('Point Deposition','Point Deposition'),
+                          ('User Defined Terrestrial Area','User Defined Terrestrial Area'))
 Aquatic_type_CHOICES=(('0','Make a selection'),
                       ('EPA Defined Pond','EPA Defined Pond'),
-                      ('EPA Defined Wetland', 'EPA Defined Wetland'))
+                      ('EPA Defined Wetland', 'EPA Defined Wetland'),
+                      ('User Defined Waterbody', 'User Defined Waterbody'))
 Calculation_input_CHOICES=(('0','Make a selection'),
                            ('Distance','Distance to waterbody or field'),
                            ('Fraction','Fraction of applied'),
@@ -42,7 +48,10 @@ Calculation_input_CHOICES=(('0','Make a selection'),
                            ('Initial Average Deposition (lb/ac)', 'Initial Average Deposition (lb/ac)'),
                            ('Initial Average Concentration (ng/L)', 'Initial Average Concentration (ng/L)'),
                            ('Initial Average Deposiion (mg/cm2)','Initial Average Deposition (mg/cm2)'))
-SELECT_VERSION = (('1.0','1.0'),)
+Version_CHOICES = (('0','Make a selection'),
+                   ('1.0','1.0'),
+                   ('2.1.1', '2.1.1'),
+                   ('3.0', '3.0'))
 #Aquatic Assessment
 #Waterbody_type_CHOICES=(('','Make a selection'),('EPA Pond','EPA Pond'),('Lake', 'Lake'), ('Watercourse', 'Watercourse')) #Aquatic Assessment
 #Orchard_CHOICES=(('','Make a selection'),('Vineyard in leaf','Vineyard in leaf'),('Orchard or dormant vineyard','Orchard or dormant vineyard'))
@@ -51,9 +60,9 @@ SELECT_VERSION = (('1.0','1.0'),)
 class AgdriftInp(forms.Form):
 
     version = forms.ChoiceField(
-        choices=SELECT_VERSION,
         label='Version',
-        initial='1.2.2')
+        choices=Version_CHOICES,
+        initial='2.1.1')
     chemical_name = forms.CharField(
         widget=forms.Textarea (attrs={'cols': 30, 'rows': 1}),
         label='Chemical Name',
@@ -67,6 +76,26 @@ class AgdriftInp(forms.Form):
         choices=Application_method_CHOICES,
         initial='Tier I Aerial',
         validators=[validation.validate_choicefield])
+    drop_size_aerial = forms.ChoiceField(
+        label='Drop Size Distribution-Aerial',
+        choices=Drop_size_distribution_aerial_CHOICES,
+        initial='Fine to Medium',
+        validators=[validation.validate_choicefield])
+    drop_size_ground = forms.ChoiceField(
+        label='Drop Size Distribution-Ground',
+        choices=Drop_size_distribution_ground_CHOICES,
+        initial='Very Fine to Fine',
+        validators=[validation.validate_choicefield])
+    boom_height = forms.ChoiceField(
+        label='Boom height',
+        choices=Boom_height_CHOICES,
+        initial='High',
+        validators=[validation.validate_choicefield])
+    airblast_type = forms.ChoiceField(
+        label='Orchard/Airblast Type',
+        choices=Orchard_CHOICES,
+        initial='Orchard',
+        validators=[validation.validate_choicefield])
     ecosystem_type = forms.ChoiceField(
         label='Ecosystem type',
         choices=Ecosystem_type_CHOICES,
@@ -77,27 +106,45 @@ class AgdriftInp(forms.Form):
         choices=Aquatic_type_CHOICES,
         initial='EPA Defined Pond',
         validators=[validation.validate_choicefield])
-    drop_size = forms.ChoiceField(
-        label='Drop Size Distribution',
-        choices=Drop_size_distribution_CHOICES,
-        initial='Medium to Coarse',
+    terrestrial_field_type = forms.ChoiceField(
+        label='Terrestrial Assessment Type',
+        choices=Terrestrial_type_CHOICES,
+        initial='Point Deposition',
         validators=[validation.validate_choicefield])
+    epa_pond_width = forms.FloatField(
+        label=mark_safe('EPA Defined Pond Width (ft)'),
+        initial='208.7',
+        validators=[validation.validate_greaterthan0])
+    epa_pond_depth = forms.FloatField(
+        label=mark_safe('EPA Defined Pond depth (ft)'),
+        initial='6.56',
+        validators=[validation.validate_greaterthan0])
+    epa_wetland_width = forms.FloatField(
+        label=mark_safe('EPA Defined Wetland Width (ft)'),
+        initial='208.7',
+        validators=[validation.validate_greaterthan0])
+    epa_wetland_depth = forms.FloatField(
+        label=mark_safe('EPA Defined Wetland depth (ft)'),
+        initial='0.4921',
+        validators=[validation.validate_greaterthan0])
+    user_waterbody_width = forms.FloatField(
+        label=mark_safe('User Defined Waterbody Width (ft)'),
+        initial='208.7',
+        validators=[validation.validate_greaterthan0])
+    user_waterbody_depth = forms.FloatField(
+        label=mark_safe('User Defined Wtaerbody depth (ft)'),
+        initial='6.56',
+        validators=[validation.validate_greaterthan0])
+    user_terrestrial_width = forms.FloatField(
+        label=mark_safe('Downwind width of Terrestrial Area (ft)'),
+        initial='208.7',
+        validators=[validation.validate_greaterthan0])
     application_rate = forms.FloatField(
         label=mark_safe('Active rate (lb/ac)'),
         initial='0.5',
         validators=[validation.validate_greaterthan0])
-    boom_height = forms.ChoiceField(
-        label='Boom height',
-        choices=Boom_height_CHOICES,
-        initial='High',
-        validators=[validation.validate_choicefield])
-    airblast_type = forms.ChoiceField(
-        label='Orchard-Airblast Type',
-        choices=Orchard_CHOICES,
-        initial='Orchard',
-        validators=[validation.validate_choicefield])
     calculation_input = forms.ChoiceField(
-        label = 'Toolbox Input Type',
+        label = 'Initial Input Type for Calculation',
         choices=Calculation_input_CHOICES,
         initial ='Distance',
         validators=[validation.validate_choicefield])
@@ -105,31 +152,10 @@ class AgdriftInp(forms.Form):
         label=mark_safe('Distance to water body or terrestrial point from edge of field (ft)'),
         initial='225',
         validators=[validation.validate_integer, validation.validate_greaterthan0])
-    user_pond_width = forms.FloatField(
-        label=mark_safe('Pond Width (ft)'),
-        initial='208.7',
-        validators=[validation.validate_greaterthan0])
-    user_pond_depth = forms.FloatField(
-        label=mark_safe('Pond depth (ft)'),
-        initial='6.56',
-        validators=[validation.validate_greaterthan0])
-    user_wetland_width = forms.FloatField(
-        label=mark_safe('Wetland Width (ft)'),
-        initial='208.7',
-        validators=[validation.validate_greaterthan0])
-    user_wetland_depth = forms.FloatField(
-        label=mark_safe('Wetland depth (ft)'),
-        initial='0.4921',
-        validators=[validation.validate_greaterthan0])
-    terrestrial_field_type = forms.ChoiceField(
-        label='Terrestrial Field type',
-        choices=Terrestrial_type_CHOICES,
-        initial='EPA Defined Terrestrial',
-        validators=[validation.validate_choicefield])
-    user_terrestrial_width = forms.FloatField(
-        label=mark_safe('Downwind width of Area (ft)'),
-        initial='208.7',
-        validators=[validation.validate_greaterthan0])
+    user_frac_applied = forms.FloatField(
+        label=mark_safe('Fraction applied'),
+        initial='0.0314',
+        validators=[validation.validate_range01])
     user_avg_dep_gha = forms.FloatField(
         label=mark_safe('Initial Average Deposition (g/ha)'),
         initial='8.82',
@@ -139,17 +165,13 @@ class AgdriftInp(forms.Form):
         initial='6.56',
         validators=[validation.validate_greaterthan0])
     user_avg_dep_lbac = forms.FloatField(
-        label=mark_safe('Pond depth (ft)'),
+        label=mark_safe('Initial Average Deposition (lb/ac)'),
         initial='6.56',
         validators=[validation.validate_greaterthan0])
     user_avg_conc_ngl = forms.FloatField(
-        label=mark_safe('Initial Average Deposition (lb/ac)'),
+        label=mark_safe('Initial Average Deposition (ng/L)'),
         initial='0.0079',
         validators=[validation.validate_greaterthan0])
-    user_frac_applied = forms.FloatField(
-        label=mark_safe('Fraction applied'),
-        initial='0.0314',
-        validators=[validation.validate_range01])
 
 #    extending_settings = forms.ChoiceField(
             # label='Optional settings', choices=Extended_settings_CHOICES, initial='Make a selection')

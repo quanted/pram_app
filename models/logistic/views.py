8bf88fe722c492ff2d3b0b543bcf6d9f32aee9cc
akanarek <andrew.kanarek@gmail.com>
2017-06-20 11:58:17 -0400

    added static_qed to image directory

--- models/logistic/views.py
+++ models/logistic/views.py
@@ -11,9 +11,9 @@ qaqc = '<p>Will be back soon..</p>'
 #model algorithm html for algorithm page
 algorithm = '' \
             '<p>The population change rate of logistic model is:</p>'\
-            '<img src = \"/images/latex/logistic/logistic_1.png\" alt=\"Logistic_1\">'\
+            '<img src = \"/static_qed/images/latex/logistic/logistic_1.png\" alt=\"Logistic_1\">'\
             '<p>whose solution is:</p>'\
-            '<img src = \"/images/latex/logistic/logistic_2.png\" alt=\"Logistic_2\">'
+            '<img src = \"/static_qed/images/latex/logistic/logistic_2.png\" alt=\"Logistic_2\">'
 
 
 #model description html for description page
@@ -22,7 +22,7 @@ description = '<p>Developed by Belgian mathematician '\
               'Pierre Verhulst</a> (1804-1849), logistic model assumes that population change rate (r<sub>t</sub>) is not a constant.'\
               'It is related to the maximum population growth rate (r<sub>0</sub>), carrying capacity (K), and current population'\
               '(N<sub>t</sub>), which follows the following relationship: '\
-              '</p><img src = \"/images/latex/logistic/logistic_des_1.png\" alt=\"Logisitic Model\">'\
+              '</p><img src = \"/static_qed/images/latex/logistic/logistic_des_1.png\" alt=\"Logisitic Model\">'\
               '<ul class=\"bullet\">'\
               '<li> Population growth rate (r<sub>t</sub>) declines with the increase of population size (N<sub>t</sub>) </li>'\
               '<li> Population growth rate (r<sub>t</sub>) is 0 when the population (N<sub>t</sub>) reaches carrying capacity (K) </li>'\

883bc924a12dea30a0659d9251602c61477139af
akanarek <andrew.kanarek@gmail.com>
2017-06-20 15:44:40 -0400

    added static_qed to image address

--- models/fellerarley/views.py
+++ models/fellerarley/views.py
@@ -11,21 +11,21 @@ qaqc = '<p>Will be back soon..</p>'
 #model algorithm html for algorithm page
 algorithm = '' \
             '<p>The population change rate is:</p>'\
-            '<img src = "/images/latex/fellerarley/fellerarley_1.png" alt="Feller-Arley_1">'\
+            '<img src = "/static_qed/images/latex/fellerarley/fellerarley_1.png" alt="Feller-Arley_1">'\
             '<p>In the perspective of probability, the chance of remaining population N at time t + &#916;t</p>'\
             '<p>needs to meet one of the following three conditions:</p>'\
             '<ul class="nobullet">'\
             '<li>N<sub>(t-1)</sub>=N-1, and exactly one birth within the interval (t, t + &#916;):</li>'\
-            '<li><img src = "/images/latex/fellerarley/fellerarley_2.png" alt="Feller-Arley_2"></li>'\
+            '<li><img src = "/static_qed/images/latex/fellerarley/fellerarley_2.png" alt="Feller-Arley_2"></li>'\
             '<li>N<sub>(t-1)</sub>=N+1, and exactly one death within the interval (t, t + &#916;):</li>'\
-            '<li><img src = "/images/latex/fellerarley/fellerarley_3.png" alt="Feller-Arley_3"></li>'\
+            '<li><img src = "/static_qed/images/latex/fellerarley/fellerarley_3.png" alt="Feller-Arley_3"></li>'\
             '<li>N<sub>(t-1)</sub>=N, and neither birth or death occur within the interval:</li>'\
-            '<li><img src = "/images/latex/fellerarley/fellerarley_4.png" alt="Feller-Arley_4"></li>'\
+            '<li><img src = "/static_qed/images/latex/fellerarley/fellerarley_4.png" alt="Feller-Arley_4"></li>'\
             '</ul>'
 '<p>Combining the above three probabilities:</p>'\
-    '<img src = "/images/latex/fellerarley/fellerarley_5.png" alt="Feller-Arley_5">'\
+    '<img src = "/static_qed/images/latex/fellerarley/fellerarley_5.png" alt="Feller-Arley_5">'\
     '<p>The change of probability becomes: </p>'\
-    '<img src = "/images/latex/fellerarley/fellerarley_6.png" alt="Feller-Arley_6">'\
+    '<img src = "/static_qed/images/latex/fellerarley/fellerarley_6.png" alt="Feller-Arley_6">'\
 
 #model description html for description page
 description = '<p>Feller-Arley Markov process is also known as the birth-death process, which assumes:</p>'\
@@ -37,10 +37,10 @@ description = '<p>Feller-Arley Markov process is also known as the birth-death p
             'In the below figure, when a birth occurs, the process goes from state n to n + 1.'\
             'When a death occurs, the process goes from state n to state n-1. The possibility of "moving"'\
             'to another state is governed by the birth and death rates.</p>'\
-            '<img src = "/images/latex/fellerarley/BD-proces.png" alt="Feller-Arley Markov Process">'\
+            '<img src = "/static_qed/images/latex/fellerarley/BD-proces.png" alt="Feller-Arley Markov Process">'\
 
 # How model name appears on web page
-header = 'Exponential'
+header = 'Feller-Arley Markov Process'
 
 history = '<p>User History</p>'
 

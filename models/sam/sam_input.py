40242574013461360ff0c0efe7c4cbd7fb5a2131
triphook <jchook3@gmail.com>
2017-06-21 09:34:06 -0400

    New SAM frontend

--- models/sam/sam_input.py
+++ models/sam/sam_input.py
@@ -23,12 +23,12 @@ def sam_input_page(request, model='', header='', form_data=None):
     html += """<br><table class="input_table tab tab_Chemical">"""
     html += str(sam_parameters.SamInp_chem(form_data))
     html += """</table><table class="input_table tab tab_Application" style="display:none">"""
-    html += str(sam_parameters.SamInp_app(form_data))
-    html += str(sam_parameters.SamInp_app_refine(form_data))
-    html += """</table><table class="input_table tab tab_Simulation" style="display:none">"""
+    html += str(sam_parameters.SamInp_app())
+    html += """<table class="input_table tab tab_Simulation" style="display:none">"""
     html += str(sam_parameters.SamInp_sim(form_data))
     html += """</table><table class="input_table tab tab_Output" style="display:none">"""
-    html += str(sam_parameters.SamInp_output(form_data))
+    html += str(sam_parameters.SamInp_output())
+    html += render_to_string('04sam_application_table.html', {})
     html += render_to_string('04uberinput_tabbed_end_drupal.html', {})
     html += render_to_string('04ubertext_end_drupal.html', {})
     # Check if tooltips dictionary exists

096c0b53f3dfafb82317e31b7c8e638fd4725131
Tom Purucker <purucker.tom@gmail.com>
2017-06-21 13:35:04 -0400

    exponential input page working

--- models/exponential/exponential_parameters.py
+++ models/exponential/exponential_parameters.py
@@ -10,7 +10,7 @@ from django import forms
 from django.db import models
 from django.utils.safestring import mark_safe
 
-class exponentialInp(forms.Form):
+class ExponentialInp(forms.Form):
     N_o = forms.FloatField(required=True,label=mark_safe('Initial amount of individuals (N<sub>0</sub>)'),initial=100)
     rho = forms.FloatField(required=True,label=mark_safe('Initial growth rate in percent (&#961;)'),initial=4)
     T = forms.FloatField(required=True,label='Number of time intervals (T)',initial=200)

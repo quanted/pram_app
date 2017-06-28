b5adbbde116a0513ed93161b83cfccb93e0d1875
Tom Purucker <purucker.tom@gmail.com>
2017-06-27 22:13:13 -0400

    watershed map

--- views/sam_watershed.py
+++ views/sam_watershed.py
@@ -1,16 +1,24 @@
 from django.template.loader import render_to_string
 from django.http import HttpResponse
 from django.shortcuts import redirect
+import importlib
 import os
 from django.conf import settings
-import links_left
-from pisces_app import views
+from . import links_left
+# from pisces_app import views
 
+def get_model_header(model):
 
+    model_views_location = 'ubertool_app.models.' + model + '.views'
+    #import_module is py27 specific
+    viewmodule = importlib.import_module(model_views_location)
+    header = viewmodule.header
+    return header
 
 def watershed_page(request, model='pisces', header='none'):
-    header = views.header
-    x = render_to_string('pisces_watershed_map.html')
+    model = model.lstrip('/')
+    header = get_model_header(model)
+    x = render_to_string('sam_watershed_map.html')
 
     """ Returns the html of the references page for pisces. """
     html = render_to_string('01epa_drupal_header.html', {})
@@ -18,7 +26,7 @@ def watershed_page(request, model='pisces', header='none'):
     html += render_to_string('03epa_drupal_section_title.html', {})
 
     html += render_to_string('04ubertext_start_index_drupal.html', {
-        'TITLE': header + ' Watershed Explorer',
+        'TITLE': header + ' Watershed Output',
         'TEXT_PARAGRAPH': x})
 
     html += render_to_string('04ubertext_end_drupal.html', {})

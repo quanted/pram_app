94188ed52c85cd09ae93b560765577ac17989a08
dbsmith88 <dbsmith@uga.edu>
2017-12-08 08:07:55 -0600

    updated handling of failed tasks

--- views/output.py
+++ views/output.py
@@ -254,13 +254,14 @@ def output_page(request, model='none', header=''):
         task = {}
         try:
             inputs = request.POST.dict()
-            # task = requests.post('http://localhost:7777/rest/ubertool/sam/', data=inputs)
+            #task = requests.post('http://localhost:7777/rest/ubertool/sam/', data=inputs)
             task = requests.post('http://qed_nginx:7777/rest/ubertool/sam/', data=inputs)
+            task_id = json.loads(task.content.decode(encoding="utf-8").replace("//", ""))
+            return redirect('/ubertool/sam/output/status/' + task_id['task_id'])
         except Exception as ex:
             print("Error attempting to connect to flask endpoint for sam. " + str(ex))
-        task_id = json.loads(task.content.decode(encoding="utf-8").replace("//", ""))
+            return redirect('/ubertool/sam/output/status/1234567890')
         # request.Cookies['task_id'] = task_id['task_id']
-        return redirect('/ubertool/sam/output/status/' + task_id['task_id'])
 
     try:
         # Class name must be ModelInp, e.g. SipInp or TerrplantInp

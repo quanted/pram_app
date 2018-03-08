b366e710e260ef3b27d61244eae541565f8e966b
Jeff Minucci <jminucci@uga.edu>
2018-03-08 10:37:52 -0500

    stand-in input page for varroapop now functional

--- models/varroapop/varroapop_input.py
+++ models/varroapop/varroapop_input.py
@@ -6,8 +6,8 @@
 from django.template.loader import render_to_string
 
 
-def beerex_input_page(request, model='', header='', form_data=None):
-    from . import beerex_parameters
+def varroapop_input_page(request, model='', header='', form_data=None):
+    from . import varroapop_parameters
 
     html = render_to_string('04uberinput_jquery.html', {'model': model})
     html += render_to_string('04uberinput_start_drupal.html', {
@@ -15,14 +15,14 @@ def beerex_input_page(request, model='', header='', form_data=None):
         'TITLE': header},
         request=request)
     html += render_to_string('04uberinput_form.html', {
-        'FORM': beerex_parameters.BeerexInp(form_data)})
+        'FORM': varroapop_parameters.VarroaPopInp(form_data)})
     html += render_to_string('04uberinput_end_drupal.html', {})
     html += render_to_string('04ubertext_end_drupal.html', {})
     # Check if tooltips dictionary exists
     # try:
-    #     import beerex_tooltips
-    #     hasattr(beerex_tooltips, 'tooltips')
-    #     tooltips = beerex_tooltips.tooltips
+    #     import varroapop_tooltips
+    #     hasattr(varroapop_tooltips, 'tooltips')
+    #     tooltips = varroapop_tooltips.tooltips
     # except:
     #     tooltips = {}
     # html += render_to_string('05ubertext_tooltips_right.html', {'tooltips': tooltips})

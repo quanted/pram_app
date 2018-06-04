68288cfbf6ebb383cd38e468efff344ba3365ae1
dbsmith88 <dbsmith@uga.edu>
2018-06-04 14:26:24 -0500

    travis ci python 3 initial test

--- views/output.py
+++ views/output.py
@@ -255,8 +255,10 @@ def output_page(request, model='none', header=''):
         task = {}
         try:
             inputs = request.POST.dict()
-            #task = requests.post('http://localhost:7777/rest/pram/sam/', data=inputs)
-            task = requests.post('http://qed_nginx:7777/rest/pram/sam/', data=inputs)
+            if os.environ['IN_DOCKER'] == "False":
+                task = requests.post('http://localhost:7777/rest/pram/sam/', data=inputs)
+            else:
+                task = requests.post('http://qed_nginx:7777/rest/pram/sam/', data=inputs)
             task_id = json.loads(task.content.decode(encoding="utf-8").replace("//", ""))
             return redirect('/pram/sam/output/status/' + task_id['task_id'])
             # task = requests.post('http://localhost:7777/rest/pram/sam/', data=inputs)

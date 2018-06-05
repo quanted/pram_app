db4759f56e965a905731aa49b6cbe52c818edf52
dbsmith88 <dbsmith@uga.edu>
2018-06-05 11:36:32 -0500

    updated test path

--- travis_build.sh
+++ travis_build.sh
@@ -3,7 +3,7 @@ git clone https://github.com/quanted/requirements_qed.git
 
 for package in $(cat requirements_qed/requirements.txt)
 do
-    if [[ "$package" == #* ]] || [[ "$package" == *gdal* ]] || [[ "$package" == *fiona* ]]
+    if [[ "$package" == #* ]] || [[ "$package" == *gdal* ]] || [[ "$package" == *fiona* ]] || [[ "$package" == *coverage* ]] || [[ "$package" == *conda* ]]
     then
         echo "Not installing $package"
     else

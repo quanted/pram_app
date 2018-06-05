328af20a22e3ce50be3936e3d12c9a6cc5a6764c
dbsmith88 <dbsmith@uga.edu>
2018-06-05 09:48:11 -0500

    travis updates

---
+++ travis_build.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/quanted/requirements_qed.git
+
+for package in $(cat requirements_qed/requirements.txt)
+do
+    if [[ $package != #* ]] || [[ $package != *gdal* ]] || [[$package != *fiona* ]]; then
+        pip install $package
+done
